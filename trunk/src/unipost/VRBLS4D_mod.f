a34a646e8d3f58cf00f759356cc690a3daf74d99
Tricia Slovacek <tricia@ucar.edu>
2012-01-10 15:03:58 +0000

    *** BRANCH *** This is not what I meant -
    git-svn-id: https://svn-dtc-unifiedpostproc.cgd.ucar.edu/branches/UPPdev@157 3ad70301-0a67-45a7-a7d4-3cbace1a8cc5

--- trunk/src/unipost/VRBLS4D_mod.f
+++
@@ -1,9 +0,0 @@
-!   01-10-22  H CHUANG - MODIFIED TO PROCESS HYBRID MODEL OUTPUT
-!   02-04-17  BALDWIN  - MODIFIED TO INCLUDE ALL 3D ARRAYS
-      module vrbls4d
-!- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-       implicit none
-!
-      real, allocatable :: DUST(:,:,:,:)
-!
-      end module vrbls4d
