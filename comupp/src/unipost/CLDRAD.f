78b918d602bdc77f8d6b236615a44b8155a721f1
Kate Smith <smithkr@ucar.edu>
2014-03-18 15:44:13 +0000

    *** BRANCH COMMIT ***
    EMC --> DTC : mods for new Ferrier-Aligo MP
    
    r38086:
    . The following subroutines were modified to work with
    latest Ferrier-Aligo MP scheme:
    MICROINIT.f  
    CALMICT.f  
    CMASSI.f
    
    . Subroutine CLDRAD.f was modified to properly initialize 
    several cloud fields.
    
    . Subroutine CALRAD_WCLOUD_newcrtm.f was modified to
    fix a bug in computing zenith angle for non-NAM model
    and also to remove print statements.
    
    . The following variables have been changed and post regression
    test output have been updated:
    For NAM:
     1. total column, model and pressure level cloud ice and snow
     2. radar reflectivity related fields (inc echo top)
     3. visibility
     4. cold liq top/bot H
     5. total cloud bot/top P and H, and T
     6. grid scale cloud bot/top P
     7. ceiling
    For GFS:
     Simulated GOES
    
    
    
    git-svn-id: https://svn-dtc-unifiedpostproc.cgd.ucar.edu/branches/March14_sync@541 3ad70301-0a67-45a7-a7d4-3cbace1a8cc5

--- comupp/src/unipost/CLDRAD.f
+++ comupp/src/unipost/CLDRAD.f
@@ -833,6 +833,7 @@
 !
 !     TIME AVERAGED HIGH CLOUD FRACTION.
       IF (IGET(302).GT.0) THEN	  
+        GRID1=SPVAL
         DO J=JSTA,JEND
         DO I=1,IM
 	  IF(AVGCFRACH(I,J) < SPVAL)GRID1(I,J) = AVGCFRACH(I,J)*100.
@@ -873,6 +874,7 @@
 !     
 !     TOTAL CLOUD FRACTION (INSTANTANEOUS).
       IF ((IGET(161).GT.0) .OR. (IGET(260).GT.0)) THEN
+         GRID1=SPVAL
          IF(MODELNAME .EQ. 'GFS')THEN
           EGRID1=SPVAL
           TCLD=SPVAL
@@ -918,6 +920,7 @@
 !
 !     TIME AVERAGED TOTAL CLOUD FRACTION.
          IF (IGET(144).GT.0) THEN
+           GRID1=SPVAL
            IF(MODELNAME == 'GFS')THEN
 	    DO J=JSTA,JEND
             DO I=1,IM
@@ -940,18 +943,9 @@
                IF (NCFRST(I,J) .GT. 0) RSUM=ACFRST(I,J)/NCFRST(I,J)
                IF (NCFRCV(I,J) .GT. 0)                               &
      &            RSUM=MAX(RSUM, ACFRCV(I,J)/NCFRCV(I,J))
-               EGRID1(I,J) = RSUM
+               GRID1(I,J) = RSUM*100.
             ENDDO
             ENDDO
-!
-            DO J=JSTA,JEND
-            DO I=1,IM
-              IF(ABS(EGRID1(I,J)-SPVAL).GT.SMALL)                    &
-     &              GRID1(I,J) = EGRID1(I,J)*100.
-            ENDDO
-            ENDDO
-	   ELSE
-	    GRID1=SPVAL 
 	   END IF 
           IF(MODELNAME.EQ.'NMM' .OR. MODELNAME.EQ.'GFS')THEN
            ID(1:25)= 0
@@ -997,17 +991,10 @@
             DO J=JSTA,JEND
             DO I=1,IM
                IF (NCFRST(I,J).GT.0.0) THEN
-                  EGRID1(I,J) = ACFRST(I,J)/NCFRST(I,J)
+                  GRID1(I,J) = ACFRST(I,J)/NCFRST(I,J)*100.
                ELSE
-                  EGRID1(I,J) = D00
+                  GRID1(I,J) = D00
                ENDIF
-            ENDDO
-            ENDDO
-!
-            DO J=JSTA,JEND
-            DO I=1,IM
-              IF(ABS(EGRID1(I,J)-SPVAL).GT.SMALL)                        &
-     &             GRID1(I,J) = EGRID1(I,J)*100.
             ENDDO
             ENDDO
 	   END IF 
@@ -1049,22 +1036,15 @@
 !     TIME AVERAGED CONVECTIVE CLOUD FRACTION.
          IF (IGET(143).GT.0) THEN
            IF(MODELNAME /= 'NMM')THEN
-	    EGRID1=SPVAL
+	    GRID1=SPVAL
 	   ELSE  
             DO J=JSTA,JEND
             DO I=1,IM
                IF (NCFRCV(I,J).GT.0.0) THEN
-                  EGRID1(I,J) = ACFRCV(I,J)/NCFRCV(I,J)
+                  GRID1(I,J) = ACFRCV(I,J)/NCFRCV(I,J)*100.
                ELSE
-                  EGRID1(I,J) = D00
+                  GRID1(I,J) = D00
                ENDIF
-            ENDDO
-            ENDDO
-!
-            DO J=JSTA,JEND
-            DO I=1,IM
-               IF(ABS(EGRID1(I,J)-SPVAL).GT.SMALL)                &  
-     &               GRID1(I,J) = EGRID1(I,J)*100.
             ENDDO
             ENDDO
 	   END IF
