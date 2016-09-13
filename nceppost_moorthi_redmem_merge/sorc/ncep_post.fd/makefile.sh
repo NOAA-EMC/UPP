fd1b8ffd11b2889b6da23d4dc5debbd8e3ce5605
shrinivas.moorthi@noaa.gov <shrinivas.moorthi@noaa.gov>
2016-09-13 19:15:22 +0000

    after merging with trunk on Sep 09, 2016

--- nceppost_moorthi_redmem_merge/sorc/ncep_post.fd/makefile.sh
+++ nceppost_moorthi_redmem_merge/sorc/ncep_post.fd/makefile.sh
@@ -3,9 +3,9 @@ set -x
 mac=$(hostname | cut -c1-1)
 mac2=$(hostname | cut -c1-2)
 ################################# options ###############################################
- export CLEAN=NO                                 # comment this line to clean before compiling
+#export CLEAN=NO                                 # comment this line to clean before compiling
 #debug=YES                                       # turn on debug mode     - default - NO
-#make_post_lib=YES                               # create post library    - default - NO
+ make_post_lib=YES                               # create post library    - default - NO
  make_post_exec=YES                              # create post executable - default - YES
 #make_nowrf=NO                                   # compile with wrf stub instead of WRF lib
  make_nowrf=YES                                  # compile with wrf stub instead of WRF lib
