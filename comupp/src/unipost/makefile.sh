32cdeaa6fc026d3d5bce8945130e7fab6554e1eb
Kate Fossell <fossell@ucar.edu>
2017-08-08 19:18:00 +0000

    *** BRANCH Commit ***
    Updating makefiles for EMC machines.
    
    
    
    git-svn-id: https://svn-dtc-unifiedpostproc.cgd.ucar.edu/branches/Merge_r94737-r96404@817 3ad70301-0a67-45a7-a7d4-3cbace1a8cc5

--- comupp/src/unipost/makefile.sh
+++ comupp/src/unipost/makefile.sh
@@ -1,13 +1,16 @@
-#!/bin/ksh
+#!/bin/sh
 set -x
 mac=$(hostname | cut -c1-1)
 mac2=$(hostname | cut -c1-2)
 ################################# options ###############################################
-#export CLEAN=NO                                 # comment this line to clean before compiling
+ export CLEAN=NO                                 # comment this line to clean before compiling
 #debug=YES                                       # turn on debug mode     - default - NO
- make_post_lib=YES                               # create post library    - default - NO
+#make_post_lib=YES                               # create post library    - default - NO
  make_post_exec=YES                              # create post executable - default - YES
-#make_nowrf=YES                                  # compile with wrf stub instead of WRF lib
+#make_nowrf=NO                                   # compile with wrf stub instead of WRF lib
+ make_nowrf=YES                                  # compile with wrf stub instead of WRF lib
+#BMPYXML=_bmpyxml                                # use original bumpy xml file
+                                                 # make sure to clean when changing BMPXML 
 ################################# options ###############################################
 #
 if [ $mac2 = ga ] ; then                         # For GAEA
@@ -30,6 +33,7 @@ debug=${debug:-NO}
 export make_post_lib=${make_post_lib:-NO}
 export make_post_exec=${make_post_exec:-YES}
 export make_nowrf=${make_nowrf:- NO}
+export BMPYXML=${BMPYXML:-""}
 if [ $machine = wcoss ] ; then
   export NETCDFPATH="/usrx/local/NetCDF/3.6.3"
   export WRFPATH="/nwprod/sorc/wrf_shared.v1.1.0"
@@ -50,6 +54,7 @@ if [ $machine = wcoss ] ; then
   export CC=cc
   if [ $debug = YES ] ; then
     export OPTS="-O0 -openmp "
+#   export DEBUG="-g -check all -ftrapuv -convert big_endian -fp-stack-check -fstack-protector -heap-arrays -recursive -traceback"
     export DEBUG="-g -traceback -convert big_endian -ftrapuv -check bounds -check format -check output_conversion -check pointers -check uninit -fp-stack-check"
   else
     export OPTS="-O3 -convert big_endian -fp-model source -openmp -xAVX"
@@ -59,7 +64,17 @@ if [ $machine = wcoss ] ; then
   export FREE="-FR"
   export TRAPS=""
   export PROFILE=""
+  module unload ics
+  module load ics/14.0.1
 elif [ $machine = wcoss_c ] ; then
+  module unload PrgEnv-intel
+  module load PrgEnv-intel
+  module unload iobuf
+  module load iobuf
+  module unload NetCDF-intel-sandybridge/4.2
+  module load NetCDF-intel-sandybridge/3.6.3
+  module list
+  export WRFPATH="/gpfs/hps/nco/ops/nwprod/wrf_shared.v1.1.0-intel"
   export FC=ftn
   export CPP="/lib/cpp -P"
   export CPPFLAGS="-DLINUX"
@@ -68,7 +83,8 @@ elif [ $machine = wcoss_c ] ; then
     export OPTS="-O0 -openmp "
     export DEBUG="-g -traceback -convert big_endian -ftrapuv -check bounds -check format -check output_conversion -check pointers -check uninit -fp-stack-check"
   else
-    export OPTS="-O3 -convert big_endian -fp-model source -openmp -xAVX"
+    export OPTS="-O3 -convert big_endian -fp-model source -openmp"
+#   export OPTS="-O3 -convert big_endian -fp-model source -openmp -xAVX"
     export DEBUG=""
   fi
   export LIST=""
@@ -103,10 +119,13 @@ elif [ $machine = zeus ] ; then
   export PROFILE=""
 elif [ $machine = theia ] ; then
   export NETCDFPATH="/apps/netcdf/4.3.0-intel"
+# export WRFPATH="/scratch4/NCEPDEV/meso/save/Dusan.Jovic/WRFV3"
   export WRFPATH="/scratch4/NCEPDEV/global/save/Shrinivas.Moorthi/theia/nceplibs/nwprod/lib/sorc/WRFV3"
+
   export NWPROD="/scratch4/NCEPDEV/global/save/Shrinivas.Moorthi/theia/nceplibs/nwprod"
   export ipv=_v2.0.3
   export spv=""
+##export spv=_v2.0.1
   export crtmv=2.0.7
   export gfsiov=""
   export w3ev=_v2.1.0
@@ -114,10 +133,24 @@ elif [ $machine = theia ] ; then
   export xmlv=_v2.0.0
   export g2tv=""
   export baciov=_v2.1.0
+
+# export NWPROD="/scratch3/NCEPDEV/nwprod"
+# export ipv=_v2.0.0
+# export spv=_v2.0.2
+# export crtmv=2.0.6
+# export crtmv=2.1.3
+# export gfsiov=_v1.1.0
+# export w3ev=_v2.0.5
+# export w3nv=_v2.0.6
+# export xmlv=_v2.0.0
+# export g2tv=_v1.3.0
+# export baciov=_v2.0.1
+
   export XMLPATH=$NWPROD
   export IPPATH=$NWPROD
   export SPPATH=$NWPROD
   export BACIOPATH=$NWPROD/lib
+
   export FC=mpiifort
   export CPP="/lib/cpp -P"
   export CC=cc
@@ -128,6 +161,8 @@ elif [ $machine = theia ] ; then
     export DEBUG="-g -check all -ftrapuv -convert big_endian -fp-stack-check -fstack-protector -heap-arrays -recursive -traceback"
   else
     export export OPTS="-O3 -convert big_endian -traceback -g -fp-model source -openmp"
+#   export export OPTS="-O2 -convert big_endian -traceback -g -fp-model source "
+#   export export OPTS="-O2 -convert big_endian -traceback -g -fp-model source -openmp"
     export DEBUG=""
   fi
   export LIST=""
@@ -135,8 +170,10 @@ elif [ $machine = theia ] ; then
   export TRAPS=""
   export PROFILE=""
 elif [ $machine = gaea ] ; then
+# export NETCDFPATH="/opt/cray/netcdf/4.1.1.0/netcdf-intel"
   export NETCDFPATH="/opt/cray/netcdf/4.3.2/INTEL/140"
   export WRFPATH="/lustre/f1/unswept/ncep/Shrinivas.Moorthi/nceplibs/nwprod/lib/sorc/WRFV3"
+# export WRFPATH="/lustre/f1/unswept/ncep/Shrinivas.Moorthi/nceplibs/nwprod/lib/sorc/wrf_shared.v1.1.0"
   export NWPROD="/lustre/f1/unswept/ncep/Shrinivas.Moorthi/nceplibs/nwprod"
   export IPPATH=$NWPROD
   export SPPATH=$NWPROD
@@ -167,45 +204,68 @@ elif [ $machine = gaea ] ; then
   export w3ev=_v2.1.0
   export w3nv=""
 fi
+#export gfsiov=${gfsiov:-_v1.1.0}
 export crtmv=${crtmv:-2.0.7}
 export crtmv_inc=${crtmv_inc:-v$crtmv}
 export XMLPATH=${XMLPATH:-$NWPROD}
 export BACIOPATH=${BACIOPATH:-$NWPROD/lib}
 export xmlv=${xmlv:-""}
 export w3ev=${w3ev:-_v2.0.3}
+#export w3nv=${w3nv:-_v2.0.3}
 export ipv=${ipv:-""}
 export spv=${spv:-""}
 
-if [ ${CLEAN:-YES}  = YES ] ; then make -f Makefile clean ; fi
+if [ ${CLEAN:-YES}  = YES ] ; then make -f Makefile_new$BMPYXML clean ; fi
 
 export CFLAGS="-DLINUX -Dfunder -DFortranByte=char -DFortranInt=int -DFortranLlong='long long'"
 if [ $machine = wcoss_c ] ; then
-
  if [ $make_nowrf = YES ] ; then
-  WRF_INC=
-  WRF_LIB=
+  export WRF_INC=""
+  export WRF_LI=""
+ else
+  export WRF_INC="-I${WRFPATH}/external/io_quilt -I${WRFPATH}/frame"
+  export WRF_LIB="${WRFPATH}/main/libwrflib.a ${WRFPATH}/frame/pack_utils.o ${WRFPATH}/frame/module_internal_header_util.o ${WRFPATH}/external/io_grib1/libio_grib1.a ${WRFPATH}/external/io_grib_share/libio_grib_share.a ${WRFPATH}/external/io_int/libwrfio_int.a ${WRFPATH}/external/io_netcdf/libwrfio_nf.a ${WRFPATH}/external/esmf_time_f90/libesmf_time.a ${WRFPATH}/external/RSL_LITE/librsl_lite.a"
  fi
  NETCDF_LIB="${NETCDF}/lib/libnetcdf.a"
- export FFLAGS="${OPTS} ${FREE} ${TRAPS} ${DEBUG} -I${XMLPARSE_INC} -I${G2_INC4} -I${G2TMPL_INC} -I${NEMSIO_INC} -I${SIGIO_INC4} -I${SFCIO_INC4} -I${GFSIO_INC4} -I${W3EMC_INC4} -I${CRTM_INC} -I${NETCDF_INCLUDE} -I${PNG_INC}"
+ export FFLAGS="${OPTS} ${FREE} ${TRAPS} ${DEBUG} ${WRF_INC} -I${XMLPARSE_INC} -I${G2_INC4} -I${G2TMPL_INC} -I${NEMSIO_INC} -I${SIGIO_INC4} -I${SFCIO_INC4} -I${GFSIO_INC4} -I${W3EMC_INC4} -I${CRTM_INC} -I${NETCDF_INCLUDE} -I${PNG_INC}"
 
- export LIBS="${WRF_LIB} ${XMLPARSE_LIB} ${G2_LIB4} ${G2TMPL_LIB} ${NEMSIO_LIB} ${GFSIO_LIB4} ${SIGIO_LIB4} ${SFCIO_LIB4} ${IP_LIB4} ${SP_LIB4} ${W3NCO_LIB4} ${W3EMC_LIB4} ${BACIO_LIB4} ${CRTM_LIB}  ${NETCDF_LIB} ${PNG_LIB} ${JASPER_LIB} ${Z_LIB}"
+#export FFLAGS="${OPTS} ${FREE} ${TRAPS} ${DEBUG} -I${WRF_INC} -I${XMLPARSE_INC} -I${G2_INC4} -I${NEMSIO_INC} -I${SIGIO_INC4} -I${SFCIO_INC4} -I${W3EMC_INC4} -I${CRTM_INC} -I${NETCDF_INCLUDE} -I${GFSIO_INC}"
+
+ export LIBS="${XMLPARSE_LIB} ${G2TMPL_LIB} ${G2_LIB4} ${JASPER_LIB} ${PNG_LIB} ${Z_LIB} ${NEMSIO_LIB} ${GFSIO_LIB4} ${SIGIO_LIB4} ${SFCIO_LIB4} ${IP_LIB4} ${SP_LIB4} ${W3EMC_LIB4} ${W3NCO_LIB4} ${BACIO_LIB4} ${CRTM_LIB} ${WRF_LIB} ${NETCDF_LDFLAGS_F}"
+
+#export LIBS=" ${WRF_LIB} ${XMLPARSE_LIB} ${G2_LIB4} ${G2TMPL_LIB} ${NEMSIO_LIB} ${GFSIO_LIB4} ${SIGIO_LIB4} ${SFCIO_LIB4} ${IP_LIB4} ${SP_LIB4} ${W3NCO_LIB4} ${W3EMC_LIB4} ${BACIO_LIB4} ${CRTM_LIB}  ${NETCDF_LIB} ${PNG_LIB} ${JASPER_LIB} ${Z_LIB}"
+#export LIBS="${XMLPARSE_LIB} ${G2_LIB4} ${G2TMPL_LIB} ${NEMSIO_LIB} ${GFSIO_LIB4} ${SIGIO_LIB4} ${SFCIO_LIB4} ${IP_LIB4} ${SP_LIB4} ${W3NCO_LIB4} ${W3EMC_LIB4} ${BACIO_LIB4} ${CRTM_LIB}  ${NETCDF_LIB} ${PNG_LIB} ${JASPER_LIB} ${Z_LIB} ${WRF_LIB}"
+
 else
  SFCIO_INC="-I${NWPROD}/lib/incmod/sfcio_4"
  SFCIO_LIB="${NWPROD}/lib/libsfcio_4.a"
 
- NEMSIO_INC="-I${NWPROD}/lib/incmod/nemsio"
- NEMSIO_LIB="-L${NWPROD}/lib -lnemsio"
+# to use new avg version
+ NEMSIO_INC="-I/nems/save/Jun.Wang/nceplibs/nemsio/nemsio_avg/incmod"
+ NEMSIO_LIB="-L/nems/save/Jun.Wang/nceplibs/nemsio/nemsio_avg -lnemsio"
+
+#NEMSIO_INC="-I${NWPROD}/lib/incmod/nemsio"
+#NEMSIO_LIB="-L${NWPROD}/lib -lnemsio"
+
  BACIO_LIB="-L${BACIOPATH} -lbacio${baciov}_4"
  SIGIO_INC="-I${NWPROD}/lib/incmod/sigio_4"
  SIGIO_LIB="${NWPROD}/lib/libsigio_4.a"
  NCDLIBS="-L${NETCDFPATH} -lnetcdf"
  NCDFFLAGS="-I${NETCDFPATH}"
  if [ $make_nowrf = YES ] ; then
-  WRF_INC=
-  WRF_LIB=
+  export WRF_INC=""
+  export WRF_LI=""
  else
-  WRF_INC="-I${WRFPATH}/external/io_quilt -I${WRFPATH}/frame"
-  WRF_LIB="${WRFPATH}/main/libwrflib.a ${WRFPATH}/frame/pack_utils.o ${WRFPATH}/frame/module_internal_header_util.o ${WRFPATH}/external/io_grib1/libio_grib1.a ${WRFPATH}/external/io_grib_share/libio_grib_share.a ${WRFPATH}/external/io_int/libwrfio_int.a ${WRFPATH}/external/io_netcdf/libwrfio_nf.a ${WRFPATH}/external/esmf_time_f90/libesmf_time.a ${WRFPATH}/external/RSL_LITE/librsl_lite.a"
+  export WRF_INC="-I${WRFPATH}/external/io_quilt -I${WRFPATH}/frame"
+  export WRF_LIB="${WRFPATH}/main/libwrflib.a                          \
+                  ${WRFPATH}/frame/pack_utils.o
+                  ${WRFPATH}/frame/module_internal_header_util.o       \
+                  ${WRFPATH}/external/io_grib1/libio_grib1.a           \
+                  ${WRFPATH}/external/io_grib_share/libio_grib_share.a \
+                  ${WRFPATH}/external/io_int/libwrfio_int.a            \
+                  ${WRFPATH}/external/io_netcdf/libwrfio_nf.a          \
+                  ${WRFPATH}/external/esmf_time_f90/libesmf_time.a     \
+                  ${WRFPATH}/external/RSL_LITE/librsl_lite.a"
  fi
 
  G2_INC="-I${NWPROD}/lib/incmod/g2_4 -I${NWPROD}/lib/incmod/g2tmpl${g2tv}"
@@ -236,23 +296,23 @@ fi
 if [ $make_post_lib = NO ] ; then
  if [ $make_post_exec = YES ] ; then
   if [ $make_nowrf = YES ] ; then
-   _make -f Makefile_nowrf
+   make -f Makefile_nowrf_new${BMPYXML}
   else
-   make -f Makefile
+   make -f Makefile_new${BMPYXML}
   fi
  fi
 else
  if [ $make_post_exec = YES ] ; then
   if [ $make_nowrf = YES ] ; then
-   make -f Makefile_nowrf
+   make -f Makefile_nowrf_new${BMPYXML}
   else
-   make -f Makefile
+   make -f Makefile_new${BMPYXML}
   fi
  fi
  export POSTLIBPATH=${POSTLIBPATH:-$(pwd)}
- if [ ${CLEAN:-YES}  = YES ] ; then rm -rf $POSTLIBPATH/include/post_4 ; fi
- mkdir -p $POSTLIBPATH/include/post_4
- make -f Makefile_lib
+ if [ ${CLEAN:-YES}  = YES ] ; then rm -rf $POSTLIBPATH/incmod/post${BMPYXML}_4 ; fi
+ mkdir -p $POSTLIBPATH/incmod/post${BMPYXML}_4
+ make -f Makefile_lib_new${BMPYXML}
 fi
 
 
