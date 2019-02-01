996bfbb7cda33d7e5d3731a80b4d667ceeada9f9
Kate Fossell <Kate.Fossell@noaa.gov>
2019-02-01 12:57:25 -0700

    Update ip libraries to v3.0.0

---
+++ comupp/src/lib/ip/iplib_8.h
@@ -0,0 +1,10 @@
+#ifndef IPLIB
+#define IPLIB
+
+void gdswzd(long *kgds, long iopt, long npts, double fill,
+            double *xpts, double *ypts, double *rlon, double *rlat,
+            long *nret,
+            double *crot, double *srot, double *xlon, double *xlat,
+            double *ylon, double *ylat, double *area);
+
+#endif
