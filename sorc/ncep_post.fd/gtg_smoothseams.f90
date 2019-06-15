c25f0ba304eee94fc907b641bcada96820c435bc
Wen Meng <wen.meng@noaa.gov>
2019-06-15 00:10:20 +0000

    VLAB Issue #44506 -- Add changes for gtg updates.

---
+++ sorc/ncep_post.fd/gtg_smoothseams.f90
@@ -0,0 +1,5 @@
+      subroutine getseams(seamfile,fillnumber,npts,ipts,jpts)
+      end subroutine getseams
+
+subroutine gtg_smoothseams(ismooth,IM,JM,jsta_2L,jend_2U,jsta,jend,fillnumber,npts,ipts,jpts,fld)
+end subroutine gtg_smoothseams
