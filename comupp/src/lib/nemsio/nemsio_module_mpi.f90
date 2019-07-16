207be01be33714f4056fb55734e81edf4a9998e7
kayee <kayee.wong@noaa.gov>
2019-07-16 11:47:27 -0600

    Nemosio fix for serial built from Laurie.

--- comupp/src/lib/nemsio/nemsio_module_mpi.f90
+++ comupp/src/lib/nemsio/nemsio_module_mpi.f90
@@ -125,9 +125,10 @@ module nemsio_module_mpi
 !
 !$$$ end module document block
 !
-  use mpi
+  !use mpi
 !
   implicit none
+  include 'mpif.h'  ! use mpif
   private
 !------------------------------------------------------------------------------
 ! private variables and type needed by nemsio_gfile
