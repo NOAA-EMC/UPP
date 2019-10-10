85d6cba83d931e973db184ff15bfbcae691c6613
Mark A Potts <Mark.Potts@noaa.gov>
2019-10-10 17:29:37 +0000

    updated for umbrella build

--- cmake/Modules/FindGEMPAK.cmake
+++
@@ -1,7 +0,0 @@
-# - Find GEMPAK 
-if(DEFINED ENV{GEMPAK})
-    set(GEMINC $ENV{GEMPAK}/include CACHE STRING "GEMPAK Include Path" )
-    set(OS_INC $ENV{OS_INC} CACHE STRING "GEMPAK OS Include Path" )
-else ()
-    message(FATAL_ERROR "Could not find GEMPAK module")
-endif()
