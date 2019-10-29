29767077b7fd60529dfb1d380fd7783878606b24
Michael Kavulich, Jr <kavulich@ucar.edu>
2019-10-29 02:49:00 +0000

    Removing the last of the comupp remnants

--- comupp/src/unipost/gtg_config.f90
+++
@@ -1,6 +0,0 @@
-module gtg_config
-contains
-  subroutine read_config()
-    print *, "Stub code for GTG protection but to make UPP public to work"
-  end subroutine read_config
-end module gtg_config
--- comupp/src/unipost/gtg_filter.f90
+++
@@ -1,6 +0,0 @@
-module gtg_filter
-contains
-  subroutine filt3d()
-    print *, "Stub code for GTG protection but to make UPP public to work"
-  end subroutine filt3d
-end module gtg_filter
