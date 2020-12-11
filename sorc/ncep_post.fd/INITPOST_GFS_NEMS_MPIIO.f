54c360a937d9bd769cd8b38cfff39c86e68f2f19
WenMeng-NOAA <48260754+WenMeng-NOAA@users.noreply.github.com>
2020-12-10 20:45:02 -0500

    The bug fix for a printout of a out of boundary array. (#239)
    * The bug fix for a printout of a out of boundary array.
    
    * Tweaking the printout line.

--- sorc/ncep_post.fd/INITPOST_GFS_NEMS_MPIIO.f
+++ sorc/ncep_post.fd/INITPOST_GFS_NEMS_MPIIO.f
@@ -1110,8 +1110,8 @@
               pint(i,j,l)   = pint(i,j,l-1) + dpres(i,j,l-1)
             enddo
           enddo
-          if (me == 0) print*,'sample model pint,pmid' ,ii,jj,l &
-          ,pint(ii,jj,l),pmid(ii,jj,l)
+          if (me == 0) print*,'sample model pint' ,ii,jj,l &
+          ,pint(ii,jj,l)
         end do
       endif
 
