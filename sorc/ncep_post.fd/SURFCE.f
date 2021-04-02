de8190a4eda0b0f369f7a2b1b1796ab63070cd5e
Wen Meng <wen.meng@noaa.gov>
2021-04-02 19:03:35 +0000

    Update logical expression operator with .eqv.

--- sorc/ncep_post.fd/SURFCE.f
+++ sorc/ncep_post.fd/SURFCE.f
@@ -3423,7 +3423,7 @@
            IF (ID(18)<0) ID(18) = 0
 
 !          print *,'IFMIN,IFHR,ITPREC',IFMIN,IFHR,ITPREC
-           if(debugprint==.true. .and. me==0)then
+           if(debugprint .eqv. .true. .and. me==0)then
              print *,'PREC_ACC_DT,ID(18),ID(19)',PREC_ACC_DT,ID(18),ID(19)
            endif
 
