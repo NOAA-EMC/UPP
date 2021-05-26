028d16adce1bdf265653106b88bc19dec17e6392
Wen Meng <wen.meng@noaa.gov>
2021-05-26 01:03:11 +0000

    Further tweaking exglobal_atmos_pmgr.sh.

--- scripts/exglobal_atmos_pmgr.sh
+++ scripts/exglobal_atmos_pmgr.sh
@@ -49,16 +49,17 @@ do
     then
       if [ $fhr -eq 0 ]
       then 
-####        ecflow_client --event release_${RUN}_postanl
         if [ -s ${COMIN}/${RUN}.${cycle}.loganl.txt ]
         then
           ecflow_client --event release_postanl
+          ecflow_client --event release_post000
+          postjobs=`echo $postjobs | sed "s/00//"`
         fi
-      fi    
-####      ecflow_client --event release_${RUN}_post${fhr}
-      ecflow_client --event release_post${fhr3}
-      # Remove current fhr from list
-      postjobs=`echo $postjobs | sed "s/${fhr}//"`
+      else    
+        ecflow_client --event release_post${fhr3}
+        # Remove current fhr from list
+        postjobs=`echo $postjobs | sed "s/${fhr}//"`
+      fi
     fi
   done
   
