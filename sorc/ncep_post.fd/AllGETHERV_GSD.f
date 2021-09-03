61e2056f791614acd0024f3ac4975b64d408a439
Bo Cui <Bo.Cui@noaa.gov>
2021-09-03 11:16:44 -0400

    20210903 Bo Cui Added new routines to 2D decomposition

--- sorc/ncep_post.fd/AllGETHERV_GSD.f
+++ sorc/ncep_post.fd/AllGETHERV_GSD.f
@@ -9,8 +9,9 @@
 !
 ! PROGRAM HISTORY LOG:
 !
+!    21-09-02  Bo Cui - Decompose UPP in X direction               
       
-     use ctlblk_mod, only : im,jm,num_procs,me,jsta,jend,mpi_comm_comp
+     use ctlblk_mod, only : im,jm,num_procs,me,jsta,jend,ista,iend,mpi_comm_comp
 
      implicit none
 
@@ -22,11 +23,11 @@
 
      REAL GRID1(IM,JM)
      REAL ibufrecv(IM*JM)
-     REAL ibufsend(im*(jend-jsta+1))
+     REAL ibufsend((iend-ista+1)*(jend-jsta+1))
      integer SENDCOUNT,RECVCOUNTS(num_procs),DISPLS(num_procs)
 !
 !     write(*,*) 'check mpi', im,jm,num_procs,me,jsta,jend
-     SENDCOUNT=im*(jend-jsta+1)
+     SENDCOUNT=(iend-ista+1)*(jend-jsta+1)
      call MPI_ALLGATHER(SENDCOUNT, 1, MPI_INTEGER, RECVCOUNTS,1 , &
                 MPI_INTEGER, mpi_comm_comp, ierr)
      DISPLS(1)=0
@@ -40,7 +41,7 @@
      ij=0
      ibufsend=0.0
      do j=jsta,jend
-        do i=1,IM
+        do i=ista,iend
            ij=ij+1
            ibufsend(ij)=GRID1(i,j)
         enddo
@@ -54,7 +55,7 @@
 
      ij=0
      do j=1,JM
-        do i=1,IM
+        do i=1,IM             
            ij=ij+1
            GRID1(i,j)=ibufrecv(ij)
         enddo
