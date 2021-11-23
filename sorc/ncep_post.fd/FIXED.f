3cc969abeb5df39a1d46b1f1e0c5daf060392cf8
wx20hc <Hui-Ya.Chuang@m71a1.ncep.noaa.gov>
2021-11-23 21:34:54 +0000

    Change sea ice threshold for identifying sea ice from .1 to 0

--- sorc/ncep_post.fd/FIXED.f
+++ sorc/ncep_post.fd/FIXED.f
@@ -110,7 +110,7 @@
            DO I = 1,IM
              GRID1(I,J) = SPVAL
               IF(SM(I,J)   /= SPVAL) GRID1(I,J) = 1. - SM(I,J)
-              IF(SICE(I,J) /= SPVAL .AND. SICE(I,J) > 0.1) GRID1(I,J) = 0.
+              IF(SICE(I,J) /= SPVAL .AND. SICE(I,J) > 0.0) GRID1(I,J) = 0.
 !           if(j==jm/2)print*,'i,mask= ',i,grid1(i,j)
            ENDDO
          ENDDO
