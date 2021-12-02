e59b906cfd80afd71cf19bd5c5a0b0909b65cf3b
HuiyaChuang-NOAA <56728916+HuiyaChuang-NOAA@users.noreply.github.com>
2021-12-02 15:27:04 -0500

    Add tref (#419)
    * Add foundation temperature per request from coupling group
    
    * add founfation T to inline post control file
    
    * add founfation T to inline post control file flux file part instead
    
    * Change sea ice threshold for identifying sea ice from .1 to 0
    
    * made updates to only use new ice fraction threshold to identify sea
    ice grid point for GFS and RRFS so that legacy models land masks
    won't be affected by this change.
    
    Co-authored-by: wx20hc <Hui-Ya.Chuang@m71a1.ncep.noaa.gov>
    Co-authored-by: wx20hc <Hui-Ya.Chuang@m71a2.ncep.noaa.gov>
    Co-authored-by: wx20hc <Hui-Ya.Chuang@m71a3.ncep.noaa.gov>

--- sorc/ncep_post.fd/VRBLS2D_mod.f
+++ sorc/ncep_post.fd/VRBLS2D_mod.f
@@ -82,7 +82,7 @@
       ,avgesnow(:,:),avgpotevp(:,:),avgprec_cont(:,:),avgcprate_cont(:,:)&
       ,ti(:,:),aod550(:,:),du_aod550(:,:),ss_aod550(:,:),su_aod550(:,:)      &
       ,bc_aod550(:,:),oc_aod550(:,:),landfrac(:,:),paha(:,:),pahi(:,:)       &
-      ,tecan(:,:),tetran(:,:),tedir(:,:),twa(:,:)
+      ,tecan(:,:),tetran(:,:),tedir(:,:),twa(:,:),fdnsst(:,:)
       integer, allocatable :: IVGTYP(:,:),ISLTYP(:,:),ISLOPE(:,:) &
       ,IEQL(:,:)
       
