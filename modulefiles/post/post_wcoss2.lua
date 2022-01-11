814077d46f3846f0e4cf486a3545f831ecacaf5d
WenMeng-NOAA <48260754+WenMeng-NOAA@users.noreply.github.com>
2022-01-11 15:32:40 -0500

    Update release/gfs_v16 with the feedback from NCO (#433)
    * Update with the feedback from NCO.
    
    * Remove envvar and update craype version

--- modulefiles/post/post_wcoss2.lua
+++ modulefiles/post/post_wcoss2.lua
@@ -2,12 +2,10 @@ help([[
 Load environment to build post on WCOSS2
 ]])
 
-envvar_ver=os.getenv("envvar_ver") or "1.0"
 PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
 intel_ver=os.getenv("intel_ver") or "19.1.3.304"
-craype_ver=os.getenv("craype_ver") or "2.7.8"
+craype_ver=os.getenv("craype_ver") or "2.7.10"
 cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
-load(pathJoin("envvar", envvar_ver))
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
 load(pathJoin("craype", craype_ver))
@@ -25,7 +23,7 @@ load(pathJoin("jasper", jasper_ver))
 load(pathJoin("libpng", libpng_ver))
 load(pathJoin("zlib", zlib_ver))
 
-g2_ver=os.getenv("g2_ver") or "3.4.4"
+g2_ver=os.getenv("g2_ver") or "3.4.5"
 g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.9.1"
 w3nco_ver=os.getenv("w3nco_ver") or "2.4.1"
 bacio_ver=os.getenv("bacio_ver") or "2.4.1"
@@ -33,7 +31,7 @@ gfsio_ver=os.getenv("gfsio_ver") or "1.4.1"
 ip_ver=os.getenv("ip_ver") or "3.3.3"
 sp_ver=os.getenv("sp_ver") or "2.3.3"
 crtm_ver=os.getenv("crtm_ver") or "2.3.0"
-w3emc_ver=os.getenv("w3emc_ver") or "2.7.3"
+w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
 load(pathJoin("g2", g2_ver))
 load(pathJoin("g2tmpl", g2tmpl_ver))
 load(pathJoin("w3nco", w3nco_ver))
