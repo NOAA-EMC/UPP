cdd328d9309c32fb4b36aa704dea269c8d14870b
EdwardColon-NOAA <edward.colon@noaa.gov>
2022-04-28 10:42:58 -0400

    Updates so that STP/SRH/UESH/VESH fields can be generated for FV3R

--- sorc/ncep_post.fd/MISCLN.f
+++ sorc/ncep_post.fd/MISCLN.f
@@ -3447,7 +3447,6 @@
 
 
       IF (SUBMODELNAME == 'RTMA')THEN
-
 !
 ! --- Effective (inflow) Layer (EL)
 !
@@ -3560,25 +3559,28 @@
 !       
 !    CAPE AND CINS 0-3KM, FOLLOW ML PROCEDURE WITH HEIGHT 0-3KM
 !
-         FIELD1=.FALSE.
-         FIELD2=.FALSE.
-!
-         IF(IGET(032)>0)THEN
-           IF(LVLS(3,IGET(032))>0)FIELD1=.TRUE.
-         ENDIF
-         IF(IGET(107)>0)THEN
-           IF(LVLS(3,IGET(107))>0)FIELD2=.TRUE.
-         ENDIF
-!
-         IF(IGET(950)>0)THEN
+         IF (MODELNAME == 'RAPR') THEN 
+           FIELD1=.FALSE.
+           FIELD2=.FALSE.
+
+           IF(IGET(032)>0)THEN
+             IF(LVLS(3,IGET(032))>0)FIELD1=.TRUE.
+           ENDIF
+           IF(IGET(107)>0)THEN
+             IF(LVLS(3,IGET(107))>0)FIELD2=.TRUE.
+           ENDIF
+
+           IF(IGET(950)>0)THEN
+             FIELD1=.TRUE.
+           ENDIF
+           IF(IGET(951)>0)THEN
+             FIELD2=.TRUE.
+           ENDIF
+
+         ELSE !FV3R and others
            FIELD1=.TRUE.
-         ENDIF
-         IF(IGET(951)>0)THEN
            FIELD2=.TRUE.
          ENDIF
-!
-!         IF(FIELD1)ITYPE=2
-!         IF(FIELD2)ITYPE=2
 
          IF(FIELD1.OR.FIELD2)THEN
            ITYPE = 2
