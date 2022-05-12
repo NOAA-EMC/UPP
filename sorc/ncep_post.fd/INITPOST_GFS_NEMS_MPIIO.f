a88d252bdd4bd698b3dff1008902a252c86ea1a7
wx22mj <Jesse.Meng@v72a1.ncep.noaa.gov>
2022-05-12 16:31:02 +0000

    20220512 Jesse Meng minor fix for INITPOST_GFS_NEMS_MPIIO.f calling EXCH

--- sorc/ncep_post.fd/INITPOST_GFS_NEMS_MPIIO.f
+++ sorc/ncep_post.fd/INITPOST_GFS_NEMS_MPIIO.f
@@ -355,7 +355,8 @@
       
       print *,me,'max(gdlat)=', maxval(gdlat),  &
                  'max(gdlon)=', maxval(gdlon)
-      CALL EXCH(gdlat(1,JSTA_2L))
+      CALL EXCH(gdlat)
+      CALL EXCH(gdlon)
       print *,'after call EXCH,me=',me
 
 !$omp parallel do private(i,j,ip1)
