42c3b1f4b785055aaca9282fbc83d463e2d6431f
eric james <eric.james@noaa.gov>
2022-11-02 16:28:01 +0000

    Adding check for model type before multiplying MSTAV by 100

--- sorc/ncep_post.fd/SURFCE.f
+++ sorc/ncep_post.fd/SURFCE.f
@@ -648,7 +648,11 @@
         DO J=JSTA,JEND
           DO I=ISTA,IEND
             IF(SMSTAV(I,J) /= SPVAL)THEN
-              GRID1(I,J) = SMSTAV(I,J)
+              IF ( MODELNAME == 'FV3R') THEN
+                GRID1(I,J) = SMSTAV(I,J)
+              ELSE
+                GRID1(I,J) = SMSTAV(I,J)*100.
+              ENDIF
             ELSE
               GRID1(I,J) = 0.
             ENDIF
