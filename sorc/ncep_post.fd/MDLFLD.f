40d607d0440d1d19bc6900f0a1db0b099e9c0b1e
weizhong zheng <weizhong.zheng@noaa.gov>
2023-02-26 21:02:39 +0000

    Revised calculation of wind GUST for UFS applications

--- sorc/ncep_post.fd/MDLFLD.f
+++ sorc/ncep_post.fd/MDLFLD.f
@@ -3774,7 +3774,7 @@ refl_adj:           IF(REF_10CM(I,J,L)<=DBZmin) THEN
 ! should only compute pblri if pblh from model is not computed based on Ri 
 ! post does not yet read pbl scheme used by model.  Will do this soon
 ! For now, compute PBLRI for non GFS models.
-              IF(MODELNAME  ==  'GFS' .OR. MODELNAME == 'FV3R')THEN
+              IF(MODELNAME  ==  'GFS')THEN
                 PBLRI=PBLH
               ELSE
                CALL CALPBL(PBLRI)
