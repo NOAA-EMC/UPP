83e59b5d92ec678bcab11dd59919fac189a11a4c
EricJames-NOAA <eric.james@noaa.gov>
2023-07-13 16:35:07 -0600

    Updating GRIB2 labeling for MSTAV, and adding SOILL on 9 RUC levels. (#740)
    * Updating GRIB2 labeling for MSTAV, and adding SOILL on 9 RUC levels.
    
    * Entering change log for INITPOST_NETCDF.f
    
    * Changing shortname of MSTAV to specify RUC LSM (defined at 0 m below ground)

--- sorc/ncep_post.fd/INITPOST_NETCDF.f
+++ sorc/ncep_post.fd/INITPOST_NETCDF.f
@@ -37,6 +37,7 @@
 !> 2023-04-21 | Eric James    | Read in / calculate some fields needed for GSL p-type diagnosis for RRFS
 !> 2023-05-31 | Wen Meng      | Bug fix in qrmax initialization
 !> 2023-06-14 | Wen Meng      ! Bug fix of reading seaswtc and modification of sndepac calculation
+!> 2023-07-06 | Eric James    ! Read in SOILL on 9 levels for RRFS
 !>
 !> @author Hui-Ya Chuang @date 2016-03-04
       SUBROUTINE INITPOST_NETCDF(ncid2d,ncid3d)
@@ -2244,6 +2245,70 @@
         enddo
       enddo
      if(debugprint)print*,'sample l',VarName,' = ',1,sh2o(isa,jsa,4)
+
+      IF (NSOIL==9) THEN
+
+      VarName='soill5'
+      call read_netcdf_2d_para(ncid2d,ista,ista_2l,iend,iend_2u,jsta,jsta_2l,jend,jend_2u, &
+      spval,VarName,sh2o(ista_2l,jsta_2l,5))
+!     mask water areas
+!$omp parallel do private(i,j)
+      do j=jsta,jend
+        do i=ista,iend
+          if (sm(i,j) /= 0.0) sh2o(i,j,5) = spval
+        enddo
+      enddo
+     if(debugprint)print*,'sample l',VarName,' = ',1,sh2o(isa,jsa,5)
+
+      VarName='soill6'
+      call read_netcdf_2d_para(ncid2d,ista,ista_2l,iend,iend_2u,jsta,jsta_2l,jend,jend_2u, &
+      spval,VarName,sh2o(ista_2l,jsta_2l,6))
+!     mask water areas
+!$omp parallel do private(i,j)
+      do j=jsta,jend
+        do i=ista,iend
+          if (sm(i,j) /= 0.0) sh2o(i,j,6) = spval
+        enddo
+      enddo
+     if(debugprint)print*,'sample l',VarName,' = ',1,sh2o(isa,jsa,6)
+
+      VarName='soill7'
+      call read_netcdf_2d_para(ncid2d,ista,ista_2l,iend,iend_2u,jsta,jsta_2l,jend,jend_2u, &
+      spval,VarName,sh2o(ista_2l,jsta_2l,7))
+!     mask water areas
+!$omp parallel do private(i,j)
+      do j=jsta,jend
+        do i=ista,iend
+          if (sm(i,j) /= 0.0) sh2o(i,j,7) = spval
+        enddo
+      enddo
+     if(debugprint)print*,'sample l',VarName,' = ',1,sh2o(isa,jsa,7)
+
+      VarName='soill8'
+      call read_netcdf_2d_para(ncid2d,ista,ista_2l,iend,iend_2u,jsta,jsta_2l,jend,jend_2u, &
+      spval,VarName,sh2o(ista_2l,jsta_2l,8))
+!     mask water areas
+!$omp parallel do private(i,j)
+      do j=jsta,jend
+        do i=ista,iend
+          if (sm(i,j) /= 0.0) sh2o(i,j,8) = spval
+        enddo
+      enddo
+     if(debugprint)print*,'sample l',VarName,' = ',1,sh2o(isa,jsa,8)
+
+      VarName='soill9'
+      call read_netcdf_2d_para(ncid2d,ista,ista_2l,iend,iend_2u,jsta,jsta_2l,jend,jend_2u, &
+      spval,VarName,sh2o(ista_2l,jsta_2l,9))
+!     mask water areas
+!$omp parallel do private(i,j)
+      do j=jsta,jend
+        do i=ista,iend
+          if (sm(i,j) /= 0.0) sh2o(i,j,9) = spval
+        enddo
+      enddo
+     if(debugprint)print*,'sample l',VarName,' = ',1,sh2o(isa,jsa,9)
+
+      END IF
 
 ! volumetric soil moisture using nemsio
       VarName='soilw1'
