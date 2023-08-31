edda2405b1872cb9106ca4ba2085a5786bf8bcae
FernandoAndrade-NOAA <Fernando.Andrade-maldonado@noaa.gov>
2023-08-31 10:59:03 -0700

    add jenkinsfile and rt-status scripts

---
+++ ci/rt-status.py
@@ -0,0 +1,56 @@
+# check the UPP RT rundir for .diff files that would indicate a change in test results
+
+'''
+Updates
+Aug/29/2023 - Fernando Andrade-Maldonado: Script creation
+
+'''
+
+import os
+
+# files used in result comparison
+tests = [
+    'nmmb',
+    'gfs',
+    'fv3gefs',
+    'fv3r',
+    'rap',
+    'hrrr',
+    'fv3hafs',
+    'rtma'
+]
+
+# look for .diff files
+# every case has its own directory in rundir
+# loop through every test case sub directory and files, then match with the test name
+def check_for_diff(tests):
+    changed = False
+    for case_dir in os.listdir('./rundir'):
+        for file in os.listdir('./rundir/{}'.format(case_dir)):
+            if file.endswith('.diff'):
+                changed = True
+                diff_case = ''
+                for test in tests:
+                    if test in case_dir:
+                        diff_case = test
+                if case_dir.endswith('pe_test'):
+                    # the rap pe test currently has a false positive bug with WRFPRS
+                    if 'rap' in case_dir and file == 'WRFPRS.GrbF16.diff':
+                        with open('./rundir/{}/{}'.format(case_dir, file)) as f:
+                            data = f.readlines()
+                            if len(data) == 1 and '708:195469472:CDCON:convective cloud layer:rpn_corr=-nan:rpn_rms=undefined' in data[0]:
+                                continue
+                    print('There are changes in results for case {}_pe_test in {}'.format(diff_case, file.replace(".diff", "")))
+                else:
+                    print('There are changes in results for case {} in {}'.format(diff_case, file.replace(".diff", "")))
+    if changed:
+        print('Refer to .diff files in rundir for details on differences for each case.')
+    else:
+        print('No changes in test results detected.')
+                
+
+def main():
+    check_for_diff(tests)
+
+if __name__ == "__main__":
+    main()
