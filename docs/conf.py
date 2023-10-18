92147aa5260da550b49bb14927ad6ef01de86bab
“gspetro-NOAA” <gillian.petro@noaa.gov>
2023-10-18 08:21:36 -0400

    add html_context to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,7 @@ html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_context = {}
 
 # Add custom style sheets
 def setup(app):
