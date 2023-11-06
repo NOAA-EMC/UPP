7f04e021fdb0cdf96e3d6c42d21b2e5965a39fe8
Wen Meng <wen.meng@noaa.gov>
2023-11-05 20:07:53 -0600

    Add changes for Hercules

--- modulefiles/hercules.lua
+++ modulefiles/hercules.lua
@@ -1,72 +1,23 @@
 help([[
-loads post prerequisites for Hercules/Intel
+Load environment to build UPP on hecules
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-dev-20230717/envs/unified-env/install/modulefiles/Core")
 
-intel_ver=os.getenv("intel_ver") or "2021.9.0"
-load(pathJoin("stack-intel", intel_ver))
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
 
-mpi_ver=os.getenv("mpi_ver") or "2021.9.0"
-load(pathJoin("stack-intel-oneapi-mpi", mpi_ver))
+stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
+load(pathJoin("stack-intel", stack_intel_ver))
+
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
-jasper_ver=os.getenv("jasper_ver") or "2.0.32"
-load(pathJoin("jasper", jasper_ver))
+load("upp_common")
 
-zlib_ver=os.getenv("zlib_ver") or "1.2.13"
-load(pathJoin("zlib", zlib_ver))
+setenv("CC","mpiicc")
+setenv("CXX","mpiicpc")
+setenv("FC","mpiifort")
 
-libpng_ver=os.getenv("libpng_ver") or "1.6.37"
-load(pathJoin("libpng", libpng_ver))
-
-hdf5_ver=os.getenv("hdf5_ver") or "1.14.1-2"
-load(pathJoin("hdf5", hdf5_ver))
-
-netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2" 
-load(pathJoin("netcdf-c", netcdf_c_ver))
-
-netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.0"
-load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
-
-bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-load(pathJoin("bacio", bacio_ver))
-
-crtm_ver=os.getenv("crtm_ver") or "2.4.0"
-load(pathJoin("crtm", crtm_ver)) 
-
-g2_ver=os.getenv("g2_ver") or "3.4.5"
-load(pathJoin("g2", g2_ver))
-
-g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.10.2"
-load(pathJoin("g2tmpl", g2tmpl_ver))
-
-ip_ver=os.getenv("ip_ver") or "3.3.3"
-load(pathJoin("ip", ip_ver))
-
-sp_ver=os.getenv("sp_ver") or "2.3.3"
-load(pathJoin("sp", sp_ver))
-
-w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
-load(pathJoin("w3emc", w3emc_ver))
-
-nemsio_ver_ver=os.getenv("nemsio_ver") or "2.5.4"
-load(pathJoin("nemsio", nemsio_ver))
-
-sigio_ver=os.getenv("sigio_ver") or "2.3.2"
-load(pathJoin("sigio", sigio_ver))
-
-sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
-load(pathJoin("sfcio", sfcio_ver))
-
-wrf_io_ver=os.getenv("wrf_io_ver") or "1.2.0"
-load(pathJoin("wrf-io", wrf_io_ver))
-
-setenv("CC", "mpiicc")
-setenv("CXX", "mpiicpc")
-setenv("FC", "mpiifort")
-setenv("CMAKE_Platform", "hercules.intel")
-
-whatis("Description: POST build environment")
+whatis("Description: UPP build environment")
