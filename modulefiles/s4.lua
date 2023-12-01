987c7f2af3064c5175b5a37e1568e5083c68bad1
souopgui <souopgui@users.noreply.github.com>
2023-12-01 09:13:27 -0600

    Update UPP modulefile on S4 with spack-stack 1.5.0 (#828)
    * Rename s4 to s4.lua, update to use spack-stack on s4
    
    * Update modulefiles for s4 to make it uniform with other platform
    
    ---------
    
    Co-authored-by: Innocent Souopgui <isouopgui@s4-submit.ssec.wisc.edu>

---
+++ modulefiles/s4.lua
@@ -0,0 +1,24 @@
+-- ---------------------------------------------------------------------------
+-- David Huber 06/2021, Set up config. with the hpc-stack NCEPLIBS.
+-- Innocent Souopgui 11/2023, Update to use spack-stack
+-- ---------------------------------------------------------------------------
+
+
+prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
+
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.0"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("cmake", cmake_ver))
+
+load("upp_common")
+
+setenv("CC","mpiicc")
+setenv("CXX","mpiicpc")
+setenv("FC","mpiifort")
+
+whatis("Description: UPP build environment")
+
