6d9f5c8172105155e41dcf3cb4ed3f48f71eba64
BijuThomas-NOAA <biju.thomas@noaa.gov>
2024-02-02 20:36:47 +0000

    Updating modules based on official installation and consistent with ufs-weather-model

--- modulefiles/wcoss2.lua
+++ modulefiles/wcoss2.lua
@@ -2,22 +2,19 @@ help([[
 Load environment to build post on WCOSS2
 ]])
 
-PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
+PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.3.3"
 intel_ver=os.getenv("intel_ver") or "19.1.3.304"
-craype_ver=os.getenv("craype_ver") or "2.7.10"
-cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
+craype_ver=os.getenv("craype_ver") or "2.7.17"
+cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.19"
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
 load(pathJoin("craype", craype_ver))
 load(pathJoin("cray-mpich", cray_mpich_ver))
 
-prepend_path("MODULEPATH", "/apps/test/hpc-stack/i-19.1.3.304__m-8.1.12__h-1.14.0__n-4.9.2__p-2.5.10__e-8.4.2/modulefiles/compiler/intel/19.1.3.304")
-prepend_path("MODULEPATH", "/apps/test/hpc-stack/i-19.1.3.304__m-8.1.12__h-1.14.0__n-4.9.2__p-2.5.10__e-8.4.2/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.12")
-
 hdf5_ver=os.getenv("hdf5_ver") or "1.14.0"
 netcdf_ver=os.getenv("netcdf_ver") or "4.9.2"
-load(pathJoin("hdf5", hdf5_ver))
-load(pathJoin("netcdf", netcdf_ver))
+load(pathJoin("hdf5-B", hdf5_ver))
+load(pathJoin("netcdf-B", netcdf_ver))
 
 jasper_ver=os.getenv("jasper_ver") or "2.0.25"
 libpng_ver=os.getenv("libpng_ver") or "1.6.37"
@@ -29,7 +26,7 @@ load(pathJoin("zlib", zlib_ver))
 g2_ver=os.getenv("g2_ver") or "3.4.5"
 g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.10.2"
 bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-ip_ver=os.getenv("ip_ver") or "3.3.3"
+ip_ver=os.getenv("ip_ver") or "4.0.0"
 sp_ver=os.getenv("sp_ver") or "2.3.3"
 crtm_ver=os.getenv("crtm_ver") or "2.4.0"
 w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
