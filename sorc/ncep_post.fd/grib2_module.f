81a85b4d04a0fee271b991a318cdbff9e43e659e
eric james <eric.james@noaa.gov>
2024-02-21 17:26:51 +0000

    Reverting changes.

--- sorc/ncep_post.fd/grib2_module.f
+++ sorc/ncep_post.fd/grib2_module.f
@@ -544,8 +544,6 @@
     integer idrsnum,numcoord,ipdsnum
     integer scaled_val_fixed_sfc2,scale_fct_fixed_sfc1
     integer scaled_val_fixed_sfc1,scale_fct_fixed_sfc2
-    integer scale_fct_prob1,scaled_val_prob1
-    integer scale_fct_prob2,scaled_val_prob2 
     character(80) fixed_sfc2_type
     integer idec_scl,ibin_scl,ibmap,inumbits
     real    fldscl
