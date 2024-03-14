d576098cbaf28f1b3c675c316798827e56d5fc2c
FernandoAndrade-NOAA <fernando.andrade-maldonado@noaa.gov>
2024-03-14 17:30:04 +0000

    hera rocky8 and hercules python

--- modulefiles/hera.lua
+++ modulefiles/hera.lua
@@ -3,7 +3,7 @@ Load environment to build UPP on hera
 ]])
 
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 load(pathJoin("stack-intel", stack_intel_ver))
