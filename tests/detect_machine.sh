92014fe2edb338f0163a156d1e1a61934c5bbfea
Fernando Andrade - NOAA <121964418+FernandoAndrade-NOAA@users.noreply.github.com>
2024-03-20 10:56:53 -0700

    Update detect_machine.sh (ufs weather model)
    Adopted detect_machine.sh from the ufs-weather-model which should be more flexible in detecting the current machine.

--- tests/detect_machine.sh
+++ tests/detect_machine.sh
@@ -1,40 +1,96 @@
 #!/bin/bash
 
+# The authoritative copy of this script lives in the ufs-weather-model at:
+# https://github.com/ufs-community/ufs-weather-model/blob/develop/tests/detect_machine.sh
+# If any local modifications are made or new platform support added,
+# please consider opening an issue and a PR to the ufs-weather-model
+# so that this copy remains in sync with its authoritative source
+#
+# Thank you for your contribution
+
+# If the MACHINE_ID variable is set, skip this script.
+[[ -n ${MACHINE_ID:-} ]] && return
+
+# First detect w/ hostname
 case $(hostname -f) in
 
-  alogin0[12].acorn.wcoss2.ncep.noaa.gov)    MACHINE_ID=wcoss2_a ;; ### acorn
-  adecflow0[12].acorn.wcoss2.ncep.noaa.gov)  MACHINE_ID=wcoss2_a ;; ### acorn
-  dlogin0[1-9].dogwood.wcoss2.ncep.noaa.gov) MACHINE_ID=wcoss2 ;; ### dodwood
-  clogin0[1-9].cactus.wcoss2.ncep.noaa.gov)  MACHINE_ID=wcoss2 ;; ### cactus
+  adecflow0[12].acorn.wcoss2.ncep.noaa.gov)   MACHINE_ID=acorn ;; ### acorn
+  alogin0[123].acorn.wcoss2.ncep.noaa.gov)    MACHINE_ID=acorn ;; ### acorn
+  clogin0[1-9].cactus.wcoss2.ncep.noaa.gov)   MACHINE_ID=wcoss2 ;; ### cactus01-9
+  clogin10.cactus.wcoss2.ncep.noaa.gov)       MACHINE_ID=wcoss2 ;; ### cactus10
+  dlogin0[1-9].dogwood.wcoss2.ncep.noaa.gov)  MACHINE_ID=wcoss2 ;; ### dogwood01-9
+  dlogin10.dogwood.wcoss2.ncep.noaa.gov)      MACHINE_ID=wcoss2 ;; ### dogwood10
 
-  gaea5[1-8])                    MACHINE_ID=gaea ;; ### gaea51-58
-  gaea5[1-8].ncrc.gov)           MACHINE_ID=gaea ;; ### gaea51-58
+  gaea5[1-8])          MACHINE_ID=gaea ;; ### gaea51-58
+  gaea5[1-8].ncrc.gov) MACHINE_ID=gaea ;; ### gaea51-58
 
-  hfe0[1-4])                   MACHINE_ID=hera_c ;; ### hera01-04
-  hfe0[5-9])                   MACHINE_ID=hera ;; ### hera05-09
-  hfe1[0-2])                   MACHINE_ID=hera ;; ### hera10-12
+  hfe0[1-4]) MACHINE_ID=hera_c ;; ### hera01-04
+  hfe0[5-9]) MACHINE_ID=hera ;;   ### hera01-09
+  hfe1[0-2]) MACHINE_ID=hera ;;   ### hera10-12
+  hecflow01) MACHINE_ID=hera ;;   ### heraecflow01
 
-  fe[1-4])                     MACHINE_ID=jet_c ;; ### jet1-4
-  fe[5-8])                     MACHINE_ID=jet ;; ### jet5-8
-  tfe[12])                     MACHINE_ID=jet ;; ### tjet1-2
+  s4-submit.ssec.wisc.edu) MACHINE_ID=s4 ;; ### s4
+
+  fe[1-4]) MACHINE_ID=jet_c ;; ### jet1-4
+  fe[5-8]) MACHINE_ID=jet ;; ### jet1-8
+  tfe[12]) MACHINE_ID=jet ;; ### tjet1-2
 
   Orion-login-[1-4].HPC.MsState.Edu) MACHINE_ID=orion ;; ### orion1-4
 
   [Hh]ercules-login-[1-4].[Hh][Pp][Cc].[Mm]s[Ss]tate.[Ee]du) MACHINE_ID=hercules ;; ### hercules1-4
 
+  derecho[1-8].hsn.de.hpc.ucar.edu) MACHINE_ID=derecho ;; ### derecho1-8
+  
   login[1-4].stampede2.tacc.utexas.edu) MACHINE_ID=stampede ;; ### stampede1-4
 
-  s4-submit.ssec.wisc.edu) MACHINE_ID=s4 ;; ### S4
+  login0[1-2].expanse.sdsc.edu) MACHINE_ID=expanse ;; ### expanse1-2
 
-  *) MACHINE_ID=unknown
+  discover3[1-5].prv.cube) MACHINE_ID=discover ;; ### discover31-35
+  *) MACHINE_ID=UNKNOWN ;;  # Unknown platform
 esac
 
-# Overwrite auto-detect with RT_MACHINE if set
-MACHINE_ID=${RT_MACHINE:-${MACHINE_ID}}
+if [[ ${MACHINE_ID} == "UNKNOWN" ]]; then 
+   case ${PW_CSP:-} in
+      "aws" | "google" | "azure") MACHINE_ID=noaacloud ;;
+      *) PW_CSP="UNKNOWN"
+   esac
+fi
 
-# Append compiler
-#if [ $MACHINE_ID = orion ] || [ $MACHINE_ID = hera ] || [ $MACHINE_ID = cheyenne ] || [ $MACHINE_ID = jet ] || [ $MACHINE_ID = gaea ] || [ $MACHINE_ID = stampede ] || [ $MACHINE_ID = s4 ]; then
-#    MACHINE_ID=${MACHINE_ID}.${RT_COMPILER}
-#fi
+# Overwrite auto-detect with MACHINE if set
+MACHINE_ID=${MACHINE:-${MACHINE_ID}}
 
-echo "Machine: " $MACHINE_ID 
+# If MACHINE_ID is no longer UNKNNOWN, return it
+if [[ "${MACHINE_ID}" != "UNKNOWN" ]]; then
+  return
+fi
+
+# Try searching based on paths since hostname may not match on compute nodes
+if [[ -d /lfs/h3 ]]; then
+  # We are on NOAA Cactus or Dogwood
+  MACHINE_ID=wcoss2
+elif [[ -d /lfs/h1 && ! -d /lfs/h3 ]]; then
+  # We are on NOAA TDS Acorn
+  MACHINE_ID=acorn
+elif [[ -d /mnt/lfs1 ]]; then
+  # We are on NOAA Jet
+  MACHINE_ID=jet
+elif [[ -d /scratch1 ]]; then
+  # We are on NOAA Hera
+  MACHINE_ID=hera
+elif [[ -d /work ]]; then
+  # We are on MSU Orion or Hercules
+  if [[ -d /apps/other ]]; then
+    # We are on Hercules
+    MACHINE_ID=hercules
+  else
+    MACHINE_ID=orion
+  fi
+elif [[ -d /gpfs && -d /ncrc ]]; then
+  # We are on GAEA.
+  MACHINE_ID=gaea
+elif [[ -d /data/prod ]]; then
+  # We are on SSEC's S4
+  MACHINE_ID=s4
+else
+  echo WARNING: UNKNOWN PLATFORM 1>&2
+fi
