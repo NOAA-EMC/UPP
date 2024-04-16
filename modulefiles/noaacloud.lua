f542c634d0fbe8f9ed87ab592318e36e6db17c14
Wei Huang <Wei.Huang@noaa.gov>
2024-04-16 22:53:54 +0000

    add module for AWS

---
+++ modulefiles/noaacloud.lua
@@ -0,0 +1,23 @@
+help([[
+Load environment to build UPP on hera
+]])
+
+
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+
+stack_intel_ver=os.getenv("stack_intel_ver") or "2021.3.0"
+load(pathJoin("stack-intel", stack_intel_ver))
+
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.3.0"
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+
+cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+load(pathJoin("cmake", cmake_ver))
+
+load("upp_common")
+
+setenv("CC","mpiicc")
+setenv("CXX","mpiicpc")
+setenv("FC","mpiifort")
+
+whatis("Description: UPP build environment")
