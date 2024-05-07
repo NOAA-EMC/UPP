ee5cbad21b38f80d05771ee176c96db2a69b3683
“gspetro-NOAA” <gillian.petro@noaa.gov>
2024-05-07 11:35:13 -0400

    RHGRD.f update

--- sorc/ncep_post.fd/RHGRD.f
+++ sorc/ncep_post.fd/RHGRD.f
@@ -1,9 +1,9 @@
 !> @file
-!> @brief module: RHGRD_mod defines the relative humidity grid _____???
+!> @brief module: RHGRD_mod defines the relative humidity threshold for onset of condensation
     module RHGRD_mod
 !-----------------------------------------------------------------------
     implicit none
 !
-      real :: RHgrd(:,:)    !< Relative humidity grid 
+      real :: RHgrd      !< Grid-scale relative humidity threshold for onset of condensation 
 !-----------------------------------------------------------------------
     end module RHGRD_mod
