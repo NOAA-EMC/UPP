a0ac84491bf2b3d7243550d38404af8a65b07184
Gillian Petro <gillian.petro@noaa.gov>
2024-06-10 19:14:56 -0400

    update svptbl.f

--- sorc/ncep_post.fd/svptbl.f
+++ sorc/ncep_post.fd/svptbl.f
@@ -1,17 +1,16 @@
 !> @file
-!> @brief module: svptbl declares variables related to saturation vapor pressure
-!tables
+!> @brief module: svptbl declares variables related to saturation vapor pressure tables
    module svptbl_mod
 !---------------------------------------------------------------------
 
     implicit none
 !
-    integer,PARAMETER :: NX=7501(:,:)         !< Determines the number of termperature values stores in the arrays__?
-    real :: C1XPVS0(:,:)                      !< Coefficients for linear interpolation of saturation vapor pressure__?
-    real :: C2XPVS0(:,:)                      !< Coefficients for linear interpolation of saturation vapor pressure__?
-    real :: C1XPVS(:,:)                       !< Additional coefficients for linear interpolation__?
-    real :: C2XPVS(:,:)                       !< Additional coefficients for linear interpolation__?
-    real :: TBPVS(NX)(:,:)                    !< Array of temperatures used for interpolation__?
-    real :: TBPVS0(NX)(:,:)                   !< Initial array of temperatures for interpolation__?
+    integer,PARAMETER :: NX=7501(:,:)         !< Table length
+    real :: C1XPVS0(:,:)                      !< Coefficient 1 for saturation vapor pressure in TBPVS0
+    real :: C2XPVS0(:,:)                      !< Coefficient 2 for saturation vapor pressure in TBPVS0
+    real :: C1XPVS(:,:)                       !< Coefficient 1 for saturation vapor pressure in TBPVS
+    real :: C2XPVS(:,:)                       !< Coefficient 2 for saturation vapor pressure in TBPVS
+    real :: TBPVS(NX)(:,:)                    !< Table of saturation vapor pressure values
+    real :: TBPVS0(NX)(:,:)                   !< Table of saturation vapor pressure values
 !
    end module svptbl_mod
