8e4300697e2b297083240bb0bfcf08ceb137d0f4
Gillian Petro <gillian.petro@noaa.gov>
2024-06-17 16:36:38 -0400

    undo removal of commented out code

--- sorc/ncep_post.fd/READ_xml.f
+++ sorc/ncep_post.fd/READ_xml.f
@@ -16,12 +16,35 @@
        use CTLBLK_mod, only: me
 !- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
        implicit none
-
+!
+!     DECLARE VARIABLES.
+!     
+!******************************************************************************
+!     START READCNTRL_XML HERE.
+!     
 !>    READ post available field table
 
       call read_postxconfig()
       num_post_afld=size(paramset(1)%param)
       num_pset=size(paramset)
+
+! LinGan below line removed because now we only read one flat file
+!
+!      if(size(post_avblflds%param)==0) then
+!        call read_xml_file_post_t( 'post_avblflds.xml')
+!        num_post_afld=size(post_avblflds%param)
+!        allocate(lvlsxml(MXLVL,num_post_afld))
+!      write(*,*)'in readxml, aft read post_avblflds.xml,num_post_afld=',num_post_afld
+!      endif
+!
+!     READ post cntrl file
+!      write(*,*)'in readxml,bf readxml,size(paramset)=',size(paramset)
+!      if(size(paramset)==0) then
+!        call read_xml_file_post_t( 'postcntrl.xml')
+!        num_pset=size(paramset)
+!        write(*,*)'in readxml, aft read postcntrl.xml,num_pset=',num_pset
+!      endif
+!
 
       RETURN
       end subroutine read_xml
