2ea75fea9809c2ac763903681f4db12ac2cd123b
Gillian Petro <gillian.petro@noaa.gov>
2024-07-02 11:42:19 -0400

    fix machine variables

--- sorc/ncep_post.fd/machine.f
+++ sorc/ncep_post.fd/machine.f
@@ -5,11 +5,11 @@
       IMPLICIT NONE
       SAVE
       
-      integer kind_io4(:,:) & !< Array of 4-byte I/O variables ?
-      ,kind_io8(:,:) &      !< Array of 8-byte I/O variables ?
-      ,kind_phys(:,:) &     !< Array of physics variables ?
-      ,kind_rad(:,:) &      !< Array of radiation variables ?
-      ,kint_mpi(:,:)        !< Array of MPI variables ?
+      integer kind_io4 &    !< 4-byte I/O variables ?
+      ,kind_io8 &           !< 8-byte I/O variables ?
+      ,kind_phys &          !< Physics variables ?
+      ,kind_rad &           !< Radiation variables ?
+      ,kint_mpi             !< MPI variables ?
       
       parameter (kind_rad = selected_real_kind(13,60)) ! the '60' maps to 64-bit real
       parameter (kind_phys = selected_real_kind(13,60)) ! the '60' maps to 64-bit real
