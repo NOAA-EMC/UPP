83d82999a0d117b41f7d1389ee2e9a2c301e15e0
Gillian Petro <gillian.petro@noaa.gov>
2024-07-02 12:06:01 -0400

    fix var defns in svptbl.f

--- sorc/ncep_post.fd/svptbl.f
+++ sorc/ncep_post.fd/svptbl.f
@@ -5,12 +5,12 @@
 
     implicit none
 !
-    integer,PARAMETER :: NX=7501(:,:)         !< Table length
-    real :: C1XPVS0(:,:)                      !< Coefficient 1 for saturation vapor pressure in TBPVS0
-    real :: C2XPVS0(:,:)                      !< Coefficient 2 for saturation vapor pressure in TBPVS0
-    real :: C1XPVS(:,:)                       !< Coefficient 1 for saturation vapor pressure in TBPVS
-    real :: C2XPVS(:,:)                       !< Coefficient 2 for saturation vapor pressure in TBPVS
-    real :: TBPVS(NX)(:,:)                    !< Table of saturation vapor pressure values
-    real :: TBPVS0(NX)(:,:)                   !< Table of saturation vapor pressure values
+    integer,PARAMETER :: NX=7501    !< Table length
+    real C1XPVS0                    !< Coefficient 1 for saturation vapor pressure in TBPVS0
+    real C2XPVS0                    !< Coefficient 2 for saturation vapor pressure in TBPVS0
+    real C1XPVS                     !< Coefficient 1 for saturation vapor pressure in TBPVS
+    real C2XPVS                     !< Coefficient 2 for saturation vapor pressure in TBPVS
+    real TBPVS(NX)                  !< Table of saturation vapor pressure values
+    real TBPVS0(NX)                 !< Table of saturation vapor pressure values
 !
    end module svptbl_mod
