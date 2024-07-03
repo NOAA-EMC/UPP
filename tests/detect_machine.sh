5ecb8d9ccc160ae8656ca933c249c4d734f42795
David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>
2024-07-03 12:09:21 -0400

    Update Orion module file for Rocky 9 upgrade (#987)
    * Update Orion module file for Rocky 9 upgrade
    
    * Update orion Intel version and detection
    
    * add Orion RT log and python
    
    * add hercules RT log
    
    * add hera RT log
    
    ---------
    
    Co-authored-by: FernandoAndrade-NOAA <fernando.andrade-maldonado@noaa.gov>

--- tests/detect_machine.sh
+++ tests/detect_machine.sh
@@ -33,7 +33,7 @@ case $(hostname -f) in
   fe[1-8]) MACHINE_ID=jet ;; ### jet1-8
   tfe[12]) MACHINE_ID=jet ;; ### tjet1-2
 
-  Orion-login-[1-4].HPC.MsState.Edu) MACHINE_ID=orion ;; ### orion1-4
+  [Oo]rion-login-[1-4].[Hh][Pp][Cc].[Mm]s[Ss]tate.[Ee]du) MACHINE_ID=orion ;; ### orion1-4
 
   [Hh]ercules-login-[1-4].[Hh][Pp][Cc].[Mm]s[Ss]tate.[Ee]du) MACHINE_ID=hercules ;; ### hercules1-4
 
@@ -76,8 +76,9 @@ elif [[ -d /scratch1 ]]; then
   # We are on NOAA Hera
   MACHINE_ID=hera
 elif [[ -d /work ]]; then
-  # We are on MSU Orion or Hercules
-  if [[ -d /apps/other ]]; then
+  # We are on MSU Orion or Hercules, check the home mount
+  mount=$(findmnt -n -o SOURCE /home)
+  if [[ ${mount} =~ "hercules" ]]; then
     # We are on Hercules
     MACHINE_ID=hercules
   else
