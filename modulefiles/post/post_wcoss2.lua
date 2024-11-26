09d9ff364d88b7eee6a732c20ebebd87619b1789
YaliMao-NOAA <53870326+YaliMao-NOAA@users.noreply.github.com>
2024-11-26 11:41:18 -0500

    Picked up module version changes from NCO based on WAFSv7.0.0 (#1092)
    Co-authored-by: yali mao <yali.mao@clogin03.cactus.wcoss2.ncep.noaa.gov>

--- modulefiles/post/post_wcoss2.lua
+++ modulefiles/post/post_wcoss2.lua
@@ -2,10 +2,10 @@ help([[
 Load environment to build post on WCOSS2
 ]])
 
-PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
+PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.3.3"
 intel_ver=os.getenv("intel_ver") or "19.1.3.304"
-craype_ver=os.getenv("craype_ver") or "2.7.10"
-cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
+craype_ver=os.getenv("craype_ver") or "2.7.17"
+cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.19"
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
 load(pathJoin("craype", craype_ver))
@@ -24,12 +24,12 @@ load(pathJoin("libpng", libpng_ver))
 load(pathJoin("zlib", zlib_ver))
 
 g2_ver=os.getenv("g2_ver") or "3.4.5"
-g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.9.1"
+g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.13.0"
 w3nco_ver=os.getenv("w3nco_ver") or "2.4.1"
 bacio_ver=os.getenv("bacio_ver") or "2.4.1"
 gfsio_ver=os.getenv("gfsio_ver") or "1.4.1"
-ip_ver=os.getenv("ip_ver") or "3.3.3"
-sp_ver=os.getenv("sp_ver") or "2.3.3"
+ip_ver=os.getenv("ip_ver") or "4.1.0"
+sp_ver=os.getenv("sp_ver") or "2.4.0"
 crtm_ver=os.getenv("crtm_ver") or "2.4.0.1"
 w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
 load(pathJoin("g2", g2_ver))
