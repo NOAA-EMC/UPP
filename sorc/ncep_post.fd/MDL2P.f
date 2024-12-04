740cba92f878c2f32e11be0414f44cefef433068
KarinaAsmar-NOAA <karina.asmar@noaa.gov>
2024-12-04 16:53:59 +0000

    fix space typo in . and.

--- sorc/ncep_post.fd/MDL2P.f
+++ sorc/ncep_post.fd/MDL2P.f
@@ -1821,7 +1821,7 @@
 !     
 !***  CHIPSI
 !
-IF ( (IGET(1021) > 0 .or. IGET(1022) > 0). and. MODELNAME == 'GFS' ) THEN
+IF ( (IGET(1021) > 0 .or. IGET(1022) > 0) .and. MODELNAME == 'GFS' ) THEN
           IF (LVLS(LP,IGET(1021)) > 0 .or. LVLS(LP,IGET(1022)) > 0) THEN
             CALL CALCHIPSI(USL,VSL,CHI,PSI)
 !         print *,'me=',me,'EGRID1=',EGRID1(1:10,JSTA)
