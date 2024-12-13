4d0f3ef0766e82b0279ac8566f95f80aa14b7b66
Biju Thomas <biju.thomas@noaa.gov>
2024-12-13 08:56:31 -0600

    Reverting to old impi version(2022.1.2) on Jet

--- modulefiles/jet.lua
+++ modulefiles/jet.lua
@@ -10,9 +10,6 @@ load(pathJoin("stack-intel", stack_intel_ver))
 stack_mpi_ver=os.getenv("stack_mpi_ver") or "2021.5.1"
 load(pathJoin("stack-intel-oneapi-mpi", stack_mpi_ver))
 
-impi_ver=os.getenv("impi_ver") or "2024.2.1"
-load(pathJoin("impi", impi_ver))
-
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
