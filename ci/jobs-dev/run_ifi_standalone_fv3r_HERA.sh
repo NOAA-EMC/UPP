af8cc99cb7c1800d9404e442372e02ec14a88ca1
Samuel Trahan (NOAA contractor) <39415369+SamuelTrahanNOAA@users.noreply.github.com>
2025-02-24 14:27:00 +0000

    Add IFI to regression tests (and put regression tests in repository) (#1119)
    * ifi-only post control file
    
    * dev jobs for ci tests, including new ifi jobs
    
    * rt.sh gets scripts from svndir and runs ifi tests
    
    * copy orion tests to jobs-dev; hercules tests are only accessible to epic
    
    * tell git to ignore emacs autosave files
    
    * more hercules jobs-dev scripts
    
    * new scripts for IFI tests on Hera
    
    * add IFI to raphrrr available fields
    
    * RRFS ifi test
    
    * add ifi regtests to rt.sh
    
    * remove unneeded file
    
    * hera test results
    
    * separate baseline for fv3r_ifi
    
    * messages for BUILD_IFI_EXECUTABLES option
    
    * fv3r_ifi_missing regtest checks if ifi has "missing" data when libifi is absent
    
    * script and resource corrections in ci/jobs-dev/run_ifi_standalone_hrrr_HERA.sh
    
    * use the ifi-only NT file for hrrr_ifi test
    
    * mkdir should be mkdir -p
    
    * revert accidental change
    
    * avoid syntax error on non-ifi platforms
    
    * Add the "ifi_missing" tests for hercules and orion
    
    * move hera rundir and homedir to sensible locations
    
    * revert post_avblflds_raphrrr.xml changes
    
    * rt.sh detects when a test could not run
    
    * update ci scripts for ifi_missing on orion and hercules
    
    * copy regtest data on msstate to test area
    
    * hera test logs
    
    * hercules logs
    
    * orion test log
    
    * add hercules rt log
    
    * add orion rt log
    
    * bug fix to runtime detection
    
    * typo fix: rtma_test_pe_test -> rtma_pe_test
    
    * rt.sh: move test_suite, and detect failures better
    
    * add hera rt log
    
    * ifi jobs should not link crtm fix files
    
    * update orion rt log
    
    * update orion rt log
    
    * update hera rt log
    
    * Update ci/rt.sh
    
    * Update ci/rt.sh
    
    * Update ci/rt.sh
    
    * update hercules rt log
    
    * update orion rt
    
    * update hera rt log
    
    ---------
    
    Co-authored-by: gspetro-NOAA <gillian.petro@noaa.gov>

---
+++ ci/jobs-dev/run_ifi_standalone_fv3r_HERA.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+#SBATCH -o out.post.ifi_standalone_fv3r
+#SBATCH -e out.post.ifi_standalone_fv3r
+#SBATCH -J ifi_standalone_fv3r_test
+#SBATCH -t 00:30:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --exclusive
+#SBATCH --partition bigmem
+#SBATCH -q batch
+#SBATCH -A ovp
+
+# specify computation resource
+export threads=40
+export MP_LABELIO=yes
+export OMP_NUM_THREADS=$threads
+export OMP_STACKSIZE=128M
+export APRUN="srun"
+
+############################################
+# Loading module
+############################################
+
+# EXPORT list here
+
+module purge
+module use /contrib/spack-stack/spack-stack-1.8.0/envs/ue-intel-2021.5.0/install/modulefiles/Core
+module load stack-intel/2021.5.0
+module load stack-intel-oneapi-mpi/2021.5.1
+module load libpng/1.6.37
+module load jasper/2.0.32
+module load prod_util/2.1.1
+module load crtm/2.4.0.1
+module load nccmp
+module load netcdf-cxx4/4.3.1
+module list
+
+ulimit -s unlimited
+ulimit
+
+msg="Starting ifi_standalone_fv3r test"
+postmsg "$logfile" "$msg"
+
+export cmp_grib2_grib2=/home/Wen.Meng/bin/cmp_grib2_grib2_new
+FIPEXEC=${svndir}/exec/fip2-lookalike.x
+
+# use the UPP run directory so we get the input files in the expected format
+export startdate=2023062800
+export DATA=$rundir/fv3r_ifi_${startdate}
+cd $DATA
+
+upp_output=cat_vars_0.nc
+ifi_standalone_output=icing-category-output.nc
+diff_file=cat_vars_0.nc.diff
+
+$APRUN --cpus-per-task=$OMP_NUM_THREADS --nodes=1 --ntasks=1 --exclusive \
+     "$FIPEXEC" -u hybr_vars_0.nc hybr_vars_0.nc .
+
+nccmp -n 20 -dfc1 -v ICE_PROB,ICE_SEV_CAT,SLD,WMO_ICE_SEV_CAT "$upp_output" "$ifi_standalone_output" 2>&1 | tee "$diff_file"
+export err1=$?
+
+if [ -s "$ifi_standalone_output" ] ; then
+ if [ $err1 -eq 0 ] && ! [ -s "$diff_file" ] ; then
+   msg="ifi standalone_fv3r test: Passed. libIFI standalone program and IFI in UPP produce identical results"
+   rm -f "$diff_file"
+   echo $msg
+ else
+   msg="ifi standalone_fv3r test: Failed. Differences detected between libIFI standalone and UPP IFI output."
+   echo $msg
+ fi
+else
+  msg="ifi standalone_fv3r test: Failed. ifi standalone failed using your new executable to generate $ifi_standalone_output"
+  echo $msg 2>&1 | tee -a TEST_ERROR
+fi
+postmsg "$logfile" "$msg"
+
+echo "PROGRAM IS COMPLETE!!!!!" 2>&1 | tee SUCCESS
+msg="Ending ifi_standalone_fv3r test"
+postmsg "$logfile" "$msg"
---
+++ ci/jobs-dev/run_post_fv3gefs_pe_test_HERCULES.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+
+#SBATCH -o out.fv3gefs_pe_test
+#SBATCH -e out.fv3gefs_pe_test
+#SBATCH -J fv3gefs_pe_test
+#SBATCH -t 00:30:00
+#SBATCH -N 4 --ntasks-per-node=12
+#SBATCH -q batch
+#SBATCH -A nems
+
+set -x
+
+# specify computation resource
+export threads=3
+export MP_LABELIO=yes
+export OMP_NUM_THREADS=$threads
+export APRUN="srun"
+
+############################################
+# Loading module
+############################################
+module use /apps/contrib/spack-stack/spack-stack-1.8.0/envs/ue-intel-2021.9.0/install/modulefiles/Core
+module load stack-intel/2021.9.0
+module load stack-intel-oneapi-mpi/2021.9.0
+module load libpng/1.6.37
+module load jasper/2.0.32
+module load prod_util/2.1.1
+module load crtm/2.4.0.1
+module list
+
+ulimit -s unlimited
+#ulimit -s1900000000
+
+msg="Starting fv3gefs test"
+postmsg "$logfile" "$msg"
+
+export cmp_grib2_grib2=/home/wmeng/bin/cmp_grib2_grib2_new
+export POSTGPEXEC=${svndir}/exec/upp.x    
+
+# specify forecast start time and hour for running your post job
+export startdate=2022042400
+export fhr=060
+export CC=`echo $startdate | cut -c9-10`
+
+# specify your running and output directory
+export DATA=$rundir/fv3gefs_${startdate}_pe_test
+rm -rf $DATA; mkdir -p $DATA
+cd $DATA
+
+export NEWDATE=`${NDATE} +${fhr} $startdate`
+                                                                                       
+export YY=`echo $NEWDATE | cut -c1-4`
+export MM=`echo $NEWDATE | cut -c5-6`
+export DD=`echo $NEWDATE | cut -c7-8`
+export HH=`echo $NEWDATE | cut -c9-10`
+
+cat > itag <<EOF
+&model_inputs
+fileName='$homedir/data_in/gefs/geaer.t${CC}z.atmf${fhr}.nemsio'
+IOFORM='binarynemsiompiio'
+grib='grib2'
+DateStr='${YY}-${MM}-${DD}_${HH}:00:00'
+MODELNAME='GFS'
+fileNameFlux='$homedir/data_in/gefs/geaer.t${CC}z.sfcf${fhr}.nemsio'
+/
+&NAMPGB
+KPO=47,PO=1000.,975.,950.,925.,900.,875.,850.,825.,800.,775.,750.,725.,700.,675.,650.,625.,600.,575.,550.,525.,500.,475.,450.,425.,400.,375.,350.,325.,300.,275.,250.,225.,200.,175.,150.,125.,100.,70.,50.,30.,20.,10.,7.,5.,3.,2.,1.,gocart_on=.true.,
+/
+EOF
+
+rm -f fort.*
+
+cp ${svndir}/fix/nam_micro_lookup.dat ./eta_micro_lookup.dat
+cp $homedir/fix/postxconfig-NT-GEFS-CHEM.txt ./postxconfig-NT.txt
+
+# copy flat files instead
+#ens_pert_type=pos_pert_fcst
+#sed < ${svndir}/parm/postxconfig-NT-GEFS.txt -e "s#negatively_pert_fcst#${ens_pert_type}#" > ./postxconfig-NT.txt
+
+cp ${svndir}/parm/params_grib2_tbl_new ./params_grib2_tbl_new
+
+cp ${svndir}/fix/chem/optics_luts_DUST.dat ./optics_luts_DUST.dat
+cp ${svndir}/fix/chem/optics_luts_SALT.dat ./optics_luts_SALT.dat
+cp ${svndir}/fix/chem/optics_luts_SOOT.dat ./optics_luts_SOOT.dat
+cp ${svndir}/fix/chem/optics_luts_SUSO.dat ./optics_luts_SUSO.dat
+cp ${svndir}/fix/chem/optics_luts_WASO.dat ./optics_luts_WASO.dat
+
+
+export PGBOUT=pgbfile
+${APRUN} ${POSTGPEXEC} < itag > outpost_nems_${NEWDATE}
+
+#$COPYGB2 -x -i'4,0,80' -k'1 3 0 7*-9999 101 0 0' $PGBOUT tfile
+#$WGRIB2 tfile -set_byte 4 11 1 -grib prmsl
+#$COPYGB2 -x -i'4,1,5' -k'1 3 5 7*-9999 100 0 50000' $PGBOUT tfile
+#$WGRIB2 tfile -set_byte 4 11 193 -grib h5wav
+#cat  prmsl h5wav >> $PGBOUT
+mv $PGBOUT geaer.t${CC}z.master.grb2f${fhr}
+
+fhr2=`printf "%02d" $fhr`
+
+filelist="geaer.t${CC}z.master.grb2f${fhr}"
+
+for file in $filelist; do
+export filein2=$file
+ls -l ${filein2}
+export err=$?
+
+if [ $err = "0" ] ; then
+
+ # use cmp to see if new pgb files are identical to the control one
+ cmp ${filein2} $homedir/data_out/gefs/${filein2}.${machine}
+
+ # if not bit-identical, use cmp_grib2_grib2 to compare each grib record
+ export err1=$?
+ if [ $err1 -eq 0 ] ; then
+  msg="fv3gefs pe test: your new post executable generates bit-identical ${filein2} as the trunk"
+  echo $msg
+ else
+  msg="fv3gefs pe test: your new post executable did not generate bit-identical ${filein2} as the trunk"
+  echo $msg
+  echo " start comparing each grib record and write the comparison result to *diff files"
+  echo " check these *diff files to make sure your new post only change variables which you intend to change"
+  $cmp_grib2_grib2 $homedir/data_out/gefs/${filein2}.${machine} ${filein2} > ${filein2}.diff
+ fi
+
+else
+
+ msg="fv3gefs pe test: post failed using your new post executable to generate ${filein2}"
+ echo $msg 2>&1 | tee -a TEST_ERROR
+
+fi
+postmsg "$logfile" "$msg"
+done
+
+echo "PROGRAM IS COMPLETE!!!!!" 2>&1 | tee SUCCESS
+msg="Ending fv3gefs pe test"
+postmsg "$logfile" "$msg"
