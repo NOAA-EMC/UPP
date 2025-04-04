7c962ef6c5937be16429eb7571603abf225e8b4e
Daniel Wesloh <22566757+DWesl@users.noreply.github.com>
2025-04-04 18:52:14 +0000

    Reduce minimum RH above 3 mbar by a factor of ten.
    Old value led to q > 10 ppm near polar stratopause.
    This version keeps specific humidity near its values in the rest of the stratosphere.

--- sorc/ncep_post.fd/UPP_PHYSICS.f
+++ sorc/ncep_post.fd/UPP_PHYSICS.f
@@ -147,7 +147,11 @@
                 Q1(I,J) = RH(I,J)*QC
               ENDIF
               IF (RH(I,J) < RHmin) THEN  !use smaller RH limit for stratosphere
-                RH(I,J) = RHmin
+                IF (P1(I, J) >= 3e2) THEN
+                  RH(I,J) = RHmin
+                ELSE IF (RH(I, J) < (RHmin / 10)) THEN
+                  RH(I, J) = RHmin / 10
+                END IF
                 Q1(I,J) = RH(I,J)*QC
               ENDIF
 !
