b486a005b1fd9781a4480a1aecb509141ad4551f
benjamin blake <benjamin.blake@clogin07.cactus.wcoss2.ncep.noaa.gov>
2025-04-18 14:15:04 +0000

    Fix typo in SURFCE.f

--- sorc/ncep_post.fd/SURFCE.f
+++ sorc/ncep_post.fd/SURFCE.f
@@ -6502,7 +6502,7 @@
            DO I=ISTA,IEND
              IF( (abs(SM(I,J)-0.)   < 1.0E-5) .AND.     &
      &           (abs(SICE(I,J)-0.) < 1.0E-5) .AND.     &
-     &           (IVGTYP(I,J) != 17)) THEN
+     &           (IVGTYP(I,J) .NE. 17)) THEN
               IF(CZMEAN(I,J)>1.E-6) THEN
                FACTRS = CZEN(I,J)/CZMEAN(I,J)
               ELSE
