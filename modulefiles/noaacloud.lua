0e78d0f3823c471e476a86b67fe4bd406d2fa298
Taylor Roper <taylor.roper@noaa.gov>
2025-05-01 15:30:08 +0000

    update build on AWS using spack-stack 1.8.0

--- modulefiles/noaacloud.lua
+++ modulefiles/noaacloud.lua
@@ -2,14 +2,19 @@ help([[
 Load environment to build UPP on NOAA Cloud
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/upp-addon-env/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/apps/modules/modulefiles")
+prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.8.0/envs/ue-intel-2021.10.0/install/modulefiles/Core")
+
 load("gnu")
-load("stack-intel")
-load("stack-intel-oneapi-mpi")
+
+stack_intel_ver = os.getenv("stack_intel_ver") or "2021.10.0"
+load(pathJoin("stack-intel", stack_intel_ver))
+
+stack_impi_ver = os.getenv("stack_impi_ver") or "2021.10.0"
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+
 unload("gnu")
 
-cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
 load("upp_common")
