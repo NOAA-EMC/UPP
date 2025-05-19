6d6ef06fcea086c30473973dc17c41e18c9ba804
Taylor Roper <taylor.roper@noaa.gov>
2025-05-19 21:32:37 +0000

    update gnu version

--- modulefiles/noaacloud.lua
+++ modulefiles/noaacloud.lua
@@ -3,16 +3,15 @@ Load environment to build UPP on NOAA Cloud
 ]])
 
 prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.8.0/envs/ue-intel-2021.10.0/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/apps/modules/modulefiles")
 
-load("gnu")
+load("gnu/9.2.0")
 
 stack_intel_ver = os.getenv("stack_intel_ver") or "2021.10.0"
 load(pathJoin("stack-intel", stack_intel_ver))
 
 stack_impi_ver = os.getenv("stack_impi_ver") or "2021.10.0"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-
-unload("gnu")
 
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
