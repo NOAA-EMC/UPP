2c8dafa78570331e6c74ae482c9f24c4cf9e43ca
Wen Meng <wen.meng@noaa.gov>
2025-05-29 16:51:03 +0000

    Option of user-defined project on WCOSS2

--- ci/submit_jobs_WCOSS2.sh
+++ ci/submit_jobs_WCOSS2.sh
@@ -10,19 +10,19 @@ cd $workdir
 for test in ${test_list}
 do
 cp $svndir/ci/jobs-dev/run_post_${test}_${machine}.sh .
-job_id=`qsub run_post_${test}_${machine}.sh`
+job_id=`qsub -A ${accnr} run_post_${test}_${machine}.sh`
 export jobid_list=$jobid_list" "${job_id}
 done
 
 #Run additional ifi tests
 if [[ "$have_ifi" == yes && "$disable_ifi" == no ]] ; then
   cp $svndir/ci/jobs-dev/run_post_hrrr_ifi_${machine}.sh .
-  job_id=`qsub run_post_hrrr_ifi_${machine}.sh`
+  job_id=`qsub -A ${accnr} run_post_hrrr_ifi_${machine}.sh`
   export jobid_list=$jobid_list" "${job_id}
   export test_list=${test_list}" hrrr_ifi"
 
   cp $svndir/ci/jobs-dev/run_post_fv3r_ifi_${machine}.sh .
-  job_id=`qsub run_post_fv3r_ifi_${machine}.sh`
+  job_id=`qsub -A ${accnr} run_post_fv3r_ifi_${machine}.sh`
   export jobid_list=$jobid_list" "${job_id}
   export test_list=${test_list}" fv3r_ifi"
 fi
