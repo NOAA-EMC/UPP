ad91bc099b0a1f0c8d96bc71fbf1380940da17a3
Samuel.Trahan <Samuel.Trahan@noaa.gov>
2025-06-06 16:43:34 +0000

    ursa default compiler = intelllvm; move defaults to top of file

--- tests/compile_upp.sh
+++ tests/compile_upp.sh
@@ -6,27 +6,13 @@
 
 set -eu
 
-usage() {
-  echo
-  echo "Usage: $0 [options]"
-  echo
-  echo "  -o  exe_name.x   Name of built UPP executable in exec. Default: upp.x"
-  echo "  -p  installation prefix <prefix>    DEFAULT: ../install"
-  echo "  -g  build with GTG(users with gtg repos. access only)     DEFAULT: OFF"
-  echo "  -i  build with libIFI(users with ifi install access only) DEFAULT: OFF"
-  echo "  -I  build with libIFI (users with ifi repos. access only) DEFAULT: OFF"
-  echo "  -B  build libIFI test programs (only valid with -I)       DEFAULT: OFF"
-  echo "  -n  build without nemsio            DEFAULT: ON"
-  echo "  -w  build without WRF-IO            DEFAULT: ON"
-  echo "  -v  build with cmake verbose        DEFAULT: OFF"
-  echo "  -c  Compiler to use for build       DEFAULT: intel"
-  echo "  -d  Debug mode of CMAKE_BUILD_TYPE  DEFAULT: Release"
-  echo "  -a  Skip deletion of exec. Add new executables. DEFAULT: OFF"
-  echo "  -Doption=value   Passes this option to cmake (can use more than once)"
-  echo "  -h  display this message and quit"
-  echo
-  exit 1
-}
+if [[ $(uname -s) == Darwin ]]; then
+  readonly MYDIR=$(cd "$(dirname "$(greadlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+else
+  readonly MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+fi
+PATHTR=${PATHTR:-$( cd ${MYDIR}/.. && pwd )}
+source ${PATHTR}/tests/detect_machine.sh
 
 delete_exec=YES
 upp_name="upp.x"
@@ -39,9 +25,37 @@ gtg_opt=" -DBUILD_WITH_GTG=OFF"
 nemsio_opt=" -DBUILD_WITH_NEMSIO=ON"
 wrfio_opt=" -DBUILD_WITH_WRFIO=ON"
 more=" "
-compiler="intel"
 verbose_opt=""
 debug_opt=""
+
+if [[ "${MACHINE_ID}" == ursa ]] ; then
+    compiler="intelllvm"
+else
+    compiler="intel"
+fi
+
+usage() {
+  echo
+  echo "Usage: $0 [options]"
+  echo
+  echo "  -o  exe_name.x   Name of built UPP executable in exec. Default: $upp_name"
+  echo "  -p  installation prefix <prefix>    DEFAULT: $prefix"
+  echo "  -g  build with GTG(users with gtg repos. access only)     DEFAULT: ${gtg_opt#*=}"
+  echo "  -i  build with libIFI(users with ifi install access only) DEFAULT: OFF"
+  echo "  -I  build with libIFI (users with ifi repos. access only) DEFAULT: OFF"
+  echo "  -B  build libIFI test programs (only valid with -I)       DEFAULT: OFF"
+  echo "  -n  build with nemsio               DEFAULT: ${nemsio_opt#*=}"
+  echo "  -w  build with WRF-IO               DEFAULT: ${wrfio_opt#*=}"
+  echo "  -v  build with cmake verbose        DEFAULT: OFF"
+  echo "  -c  Compiler to use for build       DEFAULT: $compiler"
+  echo "  -d  Debug mode of CMAKE_BUILD_TYPE  DEFAULT: Release"
+  echo "  -a  Skip deletion of exec. Add new executables. DEFAULT: OFF"
+  echo "  -Doption=value   Passes this option to cmake (can use more than once)"
+  echo "  -h  display this message and quit"
+  echo
+  exit 1
+}
+
 while getopts ":p:gnwc:vhiIdBD:o:a" opt; do
   case $opt in
     a)
@@ -99,14 +113,6 @@ if [[ ! -z $debug_opt && $ifi_opt =~ INTERNAL.*=ON ]] ; then
 fi
 
 cmake_opts=" -DCMAKE_INSTALL_PREFIX=$prefix"${nemsio_opt}${wrfio_opt}${gtg_opt}${ifi_opt}${debug_opt}${build_ifi_executables_opt}${more}
-
-if [[ $(uname -s) == Darwin ]]; then
-  readonly MYDIR=$(cd "$(dirname "$(greadlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
-else
-  readonly MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
-fi
-PATHTR=${PATHTR:-$( cd ${MYDIR}/.. && pwd )}
-source ${PATHTR}/tests/detect_machine.sh
 
 #Load required modulefiles
 if [[ $MACHINE_ID != "unknown" ]]; then
