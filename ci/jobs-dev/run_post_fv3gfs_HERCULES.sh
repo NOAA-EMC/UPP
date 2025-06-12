d9f840ea6720a3049a98433c682779ee096f0234
Gillian Petro <gillian.petro@noaa.gov>
2025-06-12 10:46:36 -0400

    update hercules job cards

--- ci/jobs-dev/run_post_fv3gfs_HERCULES.sh
+++ ci/jobs-dev/run_post_fv3gfs_HERCULES.sh
@@ -23,8 +23,7 @@ export APRUN_DWN="srun --export=ALL"
 # Loading module
 ############################################
 module use ${svndir}/modulefiles
-module load stack-oneapi/2024.2.1
-module load stack-intel-oneapi-mpi/2021.13
+module load hercules_intel
 module load libpng/1.6.37
 module load jasper/2.0.32
 module load prod_util/2.1.1
