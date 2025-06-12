263e67a6e1e623766f3e7e6e0d790482cb4c9de7
Jesse.Meng <Jesse.Meng@noaa.gov>
2025-06-12 12:55:41 -0400

    20250612 Jesse Meng fix bug in calslr_utah2 RH unit

--- sorc/ncep_post.fd/UPP_PHYSICS.f
+++ sorc/ncep_post.fd/UPP_PHYSICS.f
@@ -5010,6 +5010,7 @@
 !> -----|------------|---------
 !> 2024-11-15 | Jesse Meng | Initial
 !> 2025-04-23 | Jesse Meng | Bug fix zmid calculation in very thin layers
+!> 2025-06-12 | Jesse Meng | Bug fix RH converted from decimal to percent
 !>
 !> @author Jesse Meng @date 2024-11-15
 
@@ -5141,6 +5142,7 @@
 
       do ifd = 1,nfl
          call calrh(pfd(:,:,ifd),tfd(:,:,ifd),qfd(:,:,ifd),rhfd(:,:,ifd))
+         rhfd(:,:,ifd)=rhfd(:,:,ifd)*100.
       enddo
 
 !        compute slr
@@ -5169,6 +5171,7 @@
                   + r15*rhfd(i,j,5)+r18*rhfd(i,j,6)+r21*rhfd(i,j,7)+r24*rhfd(i,j,8) &
                   + b
          slr(i,j) = max(slr(i,j),3.)
+         if(tfd(i,j,1) >= 273.15+5.0) slr(i,j)=0.
       endif
       endif
       enddo
@@ -5207,7 +5210,7 @@
          if(twet05(i,j) > 0 .and. slr(i,j)<spval) then
             htabh=zwet(i,j)-zint(i,j,lm+1)
             if(htabh<0.) htabh=0.
-            slr(i,j)=slr(i,j)*(1.-htabh/200.)
+!            slr(i,j)=slr(i,j)*(1.-htabh/200.)
             if(slr(i,j)<0.) slr(i,j)=0.
          endif
       enddo
