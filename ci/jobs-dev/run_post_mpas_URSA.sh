0a7ac7bc446d71efcbe5cd8f42054186d4f4d7f7
Samuel.Trahan <Samuel.Trahan@noaa.gov>
2025-06-16 18:00:11 +0000

    use different path for postxconfig file in mpas test

--- ci/jobs-dev/run_post_mpas_URSA.sh
+++ ci/jobs-dev/run_post_mpas_URSA.sh
@@ -64,7 +64,7 @@ rm -f fort.*
 
 cp ${svndir}/fix/rap_micro_lookup.dat .
 cp ${svndir}/fix/nam_micro_lookup.dat .
-cp ${svndir}/parm/postxconfig-NT-MPAS.txt ./postxconfig-NT.txt
+cp ${svndir}/parm/mpas/postxconfig-NT-hfip_mpas.txt ./postxconfig-NT.txt
 cp ${svndir}/parm/params_grib2_tbl_new ./params_grib2_tbl_new
 
 #get crtm fix file
