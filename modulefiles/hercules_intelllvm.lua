49ef52ea7964129bf159457f8324e8de3ec95d57
Gillian Petro <gillian.petro@noaa.gov>
2025-06-16 17:10:02 -0400

    add llvm modulefiles for Hercules/Orion

---
+++ modulefiles/hercules_intelllvm.lua
@@ -0,0 +1,25 @@
+help([[
+Load environment to build UPP on hecules with LLVM compilers
+]])
+
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
+
+stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
+load(pathJoin("stack-oneapi", stack_intel_ver))
+
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+
+cmake_ver=os.getenv("cmake_ver") or "3.27.9"
+load(pathJoin("cmake", cmake_ver))
+
+python_ver=os.getenv("python_ver") or "3.10.8"
+load(pathJoin("python", python_ver))
+
+load("upp_common")
+
+setenv("CC","mpiicc")
+setenv("CXX","mpiicpc")
+setenv("FC","mpiifort")
+
+whatis("Description: UPP build environment")
