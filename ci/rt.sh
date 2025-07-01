70dac54a5d4798814d7b762990a0d06246eade76
benjamin blake <benjamin.blake@dlogin01.dogwood.wcoss2.ncep.noaa.gov>
2025-07-01 16:21:31 +0000

    Fix  path for WCOSS2

--- ci/rt.sh
+++ ci/rt.sh
@@ -197,7 +197,7 @@ elif [ $mac3 = herc ] ; then
  module load python/3.10.8
 elif [ $mac = d -o $mac = c ]; then #for WCOSS2
  export machine=WCOSS2
- export homedir=${homedir:-"/u/wen.meng/test_suite"}
+ export homedir=${homedir:-"/u/wen.meng/noscrub/test_suite"}
  export rundir=${rundir:-"/lfs/h2/emc/ptmp/$USER"}
  export accnr=${accnr:-"GFS-DEV"}
  module reset
