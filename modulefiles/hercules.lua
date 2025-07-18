6979c7b5de4ff8e60189c11f70132cd1e4cfe546
BijuThomas-NOAA <biju.thomas@noaa.gov>
2025-07-18 13:37:30 +0000

    Update modules/libraries to spack-stack-1.9.2

--- modulefiles/hercules.lua
+++ modulefiles/hercules.lua
@@ -3,19 +3,17 @@ Load environment to build UPP on hecules
 ]])
 
 
-prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.8.0/envs/ue-intel-2021.9.0/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/intel-oneapi-mpi/2021.13-sqiixt7/gcc/13.3.0")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
-load(pathJoin("stack-intel", stack_intel_ver))
+stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
+load(pathJoin("stack-oneapi", stack_intel_ver))
 
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
-
-python_ver=os.getenv("python_ver") or "3.10.8"
-load(pathJoin("python", python_ver))
 
 load("upp_common")
 
