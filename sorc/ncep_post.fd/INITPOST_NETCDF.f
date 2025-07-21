967e37408f519cc822f3aad2d29ab8d25faba7b8
samuel.trahan <Samuel.Trahan@noaa.gov>
2025-07-21 16:32:12 +0000

    missing .and.

--- sorc/ncep_post.fd/INITPOST_NETCDF.f
+++ sorc/ncep_post.fd/INITPOST_NETCDF.f
@@ -3050,7 +3050,7 @@
       spval,VarName,f10m)
       do j=jsta,jend
          do i=ista,iend
-            if(u10(i,j) == spval .and. v10(i,j) == spval  &
+            if(u10(i,j) == spval .and. v10(i,j) == spval .and. &
                 f10m(i,j) /=spval .and. uh(i,j,lm)/=spval) then
                u10(i,j) = f10m(i,j) * uh(i,j,LM)
                v10(i,j) = f10m(i,j) * vh(i,j,LM)
