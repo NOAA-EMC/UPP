70ffb6eeb5e145c0f1fbea69e5237d12ef122dd0
Biju Thomas <biju.thomas@noaa.gov>
2025-08-28 11:57:54 -0400

    Update the modulefile on Gaea C6(wgrib2 with support for ipolates and netcdf)

--- modulefiles/gaea_intel.lua
+++ modulefiles/gaea_intel.lua
@@ -5,13 +5,12 @@ help([[
 
 whatis([===[Loads libraries needed for building the UPP on Gaea ]===])
 
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-intel-2023.2.0/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/modulefiles")
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
-load(pathJoin("stack-intel", stack_intel_ver))
+stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 
-stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.30"
+stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.32"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
@@ -21,7 +20,7 @@ load("upp_common")
 load("zlib/1.2.13")
 
 unload("darshan-runtime")
-unload("cray-libsci")
+--unload("cray-libsci")
 
 setenv("CC","cc")
 setenv("CXX","CC")
