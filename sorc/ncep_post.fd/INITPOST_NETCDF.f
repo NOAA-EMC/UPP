0dce283831d55e303abac08e7e7149a6e0a5bb46
Christopher Hill <102273578+ChristopherHill-NOAA@users.noreply.github.com>
2025-10-08 11:23:00 -0400

    augment the added omp statement in INITPOST_NETCDF.f
    Co-authored-by: WenMeng-NOAA <48260754+WenMeng-NOAA@users.noreply.github.com>

--- sorc/ncep_post.fd/INITPOST_NETCDF.f
+++ sorc/ncep_post.fd/INITPOST_NETCDF.f
@@ -2128,7 +2128,7 @@
 
 ! Calculate (or otherwise retrieve??) the cosine of the solar zenith angle
       call w3fs13(idat(3),idat(1),idat(2),jdn)
-!$omp parallel do private(i,j)
+!$omp parallel do private(i,j,jdn,sun_zenith,sun_azimuth,temp)
       do j=jsta,jend
         do i=ista,iend
           call zensun(jdn,float(idat(4)),gdlat(i,j),gdlon(i,j),pi,sun_zenith,sun_azimuth)
