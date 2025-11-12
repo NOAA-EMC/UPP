78595dcb2067595804308c1f582a2781ea196966
Samuel.Trahan <Samuel.Trahan@noaa.gov>
2025-11-12 23:01:08 +0000

    modules for ifi tests on ursa

---
+++ modulefiles/ursa_intel_ifi_test_prereqs.lua
@@ -0,0 +1,6 @@
+help([[
+Load prerequisites for IFI's own internal test programs and its "fip2 lookalike." This is only needed with the internal IFI when building with -DBUILD_IFI_EXECUTABLES=ON (the -B option to compile_upp.sh).
+]])
+
+
+load("netcdf-cxx4/4.3.1")
