8de99d494f3c2eb77702592958b7fe2f8636aba0
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-01-21 18:10:56 -0800

    complete test for icaoheight, start test cases for geo_zenith_angle

--- unit_tests/test_geo_zenith_angle.f90
+++ unit_tests/test_geo_zenith_angle.f90
@@ -6,6 +6,49 @@
 program test_geo_zenith_angle
     implicit none
 
+    real, parameter :: tol = 1.0e-8
+    integer, parameter :: ntests = 5
+    integer :: i, res
+    real, dimension(ntests) :: RLAT, RLON, SLAT, SLON, ZA, EXP_ZA
+
+    ! Default input values
+    RLAT = 35.0
+    RLON = 135.0
+    SLAT = 0.0
+    SLON = 140.7
+
+    ! RLAT > 180.0
+    RLAT(2) = 250.0
+    
+    ! COSE clips to 1.0
+    RLAT(3) = 0.0
+    RLON(3) = 180.0
+    SLAT(3) = 0.0
+    SLON(3) = 0.0
+
+    ! COSE clips to -1.0
+    RLAT(4) = 0.0
+    RLON(4) = 0.0
+    SLAT(4) = 0.0
+    SLON(4) = 0.0    
+    
+    ! ZA clips to 0.0
+    RLAT(5) = 0.0
+    RLON(5) = 360.0
+    SLAT(5) = 0.0
+    SLON(5) = 0.0
+
+    ! TODO: Fix this later.
+    EXP_ZA = (/0.0, 0.0, 0.0, 0.0, 0.0/)
+
+    do i = 1, ntests
+        call GEO_ZENITH_ANGLE(RLAT(i), RLON(i), SLAT(i), SLON(i), ZA(i))
+    end do
+
+    res = 0
+    do i = 1, ntests
+        print '(A,I0,A,ES24.10)', "ZA(", i, ") = ", ZA(i)
+    end do
     
     print *, 'SUCCESS!'
 end program test_geo_zenith_angle
