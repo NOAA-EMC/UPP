6998f1fc08cadbf7bd839f10eb90d015d643cafe
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-01-27 11:55:17 -0800

    Update test_wetfrzlvl.f90

--- unit_tests/test_wetfrzlvl.f90
+++ unit_tests/test_wetfrzlvl.f90
@@ -91,18 +91,25 @@ program test_wetfrzlvl
 
     t(2,1,nlevs) = t_sfc
 
-
-    ! TODO: Replace ??? with code to set up a test case at (i,j) = (3,1), with the appropriate 
-    ! vertical profile, such that:
-    ! TWET(3,1,nlev) = 273.15 K and ZWET(3,1) < ZU and -ZWET(3,1) < ZU where:
-    ! ZU = 0.5*(ZINT(3,1,nlevs)+ZINT(3,1,nlevs+1))
-    ! You must ensure that the values are chosen such that we enter loopL at (I,J) = (3,1) in WETFRZLVL.
     sm(3,1)   = 0.5
     thz0(3,1) = 274.5
     ths(3,1)  = 274.5
     TWET(3,1,nlevs) = 273.15
     t(3,1,nlevs) = sm(3,1)*thz0(3,1) + (1.0 - sm(3,1))*ths(3,1) * (pint(3,1,nlevs+1)/p1000)**capa
     
+
+    ! TODO: Replace ??? with code to set up a test case at (i,j) = (3,2), with the appropriate 
+    ! vertical profile, such that:
+    ! TWET(3,1,nlev) = 273.15 K and ZWET(3,1) < ZU and -ZWET(3,1) < ZU where:
+    ! ZU = 0.5*(ZINT(3,1,nlevs)+ZINT(3,1,nlevs+1))
+    ! You must ensure that the values are chosen such that we enter loopL at (I,J) = (3,2) in WETFRZLVL.
+    ! We also require that DELT /= 0 in the calculation of ZWET.
+    sm(3,2)   = 0.5
+    thz0(3,2) = 276.0
+    ths(3,2)  = 276.0
+    TWET(3,2,nlevs) = 273.15
+    t(3,2,nlevs)    = 272.5
+
     call WETFRZLVL(TWET, ZWET)
 
     do i = ista, iend
