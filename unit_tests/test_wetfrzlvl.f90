3f5743eb4aca3ed4944fa83043a197450121171d
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-02-03 14:47:46 -0800

    Update test_wetfrzlvl.f90

--- unit_tests/test_wetfrzlvl.f90
+++ unit_tests/test_wetfrzlvl.f90
@@ -90,7 +90,6 @@ program test_wetfrzlvl
     thz0(2,1) = 281.0
     ths(2,1)  = 270.0
     t_sfc = sm(2,1)*thz0(2,1) + (1.0 - sm(2,1))*ths(2,1) * ( pint(2,1,nlevs+1) / p1000 )**capa
-    t(2,1,nlevs-1) = t_sfc
     t(2,1,nlevs) = t_sfc
     EXP_ZWET(2,1) = 9.1531143188E+01
 
@@ -124,13 +123,12 @@ program test_wetfrzlvl
     res = 0
     do i = ista, iend
         do j = jsta, jend
-        print '(A,I0,A,I0,A,ES24.10)', "EXP_ZWET(", i, ",", j, ") = ", EXP_ZWET(i,j)
-            !if ( abs(ZWET(i,j) - EXP_ZWET(i,j)) > tol ) then
-            !    print *, "Test failed at (i,j)=(", i, ",", j, "): ", &
-            !             "Expected ZWET = ", EXP_ZWET(i,j), &
-            !             ", Computed ZWET = ", ZWET(i,j)
-            !    res = 1
-            !end if
+            if ( abs(ZWET(i,j) - EXP_ZWET(i,j)) > tol ) then
+                print *, "Test failed at (i,j)=(", i, ",", j, "): ", &
+                         "Expected ZWET = ", EXP_ZWET(i,j), &
+                         ", Computed ZWET = ", ZWET(i,j)
+                res = 1
+            end if
         end do
     end do
 
