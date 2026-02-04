165671c6a14013e2b17e4280c4b24cd21496ad57
benjamin blake <benjamin.blake@clogin03.cactus.wcoss2.ncep.noaa.gov>
2026-02-04 16:57:41 +0000

    Add capecin_2m namelist option to affected model applications

--- ci/jobs-dev/run_post_mpas_hfip_WCOSS2.sh
+++ ci/jobs-dev/run_post_mpas_hfip_WCOSS2.sh
@@ -60,6 +60,9 @@ cat > itag <<EOF
     modelname = 'RAPR'
     submodelname = 'MPAS'
 /
+&NAMPGB
+    capecin_2m=.false.,
+/
 EOF
 
 # copy fix data
--- ci/jobs-dev/run_post_mpas_hfip_template.sh
+++ ci/jobs-dev/run_post_mpas_hfip_template.sh
@@ -61,6 +61,9 @@ cat > itag <<EOF
     modelname = 'RAPR'
     submodelname = 'MPAS'
 /
+&NAMPGB
+    capecin_2m=.false.,
+/
 EOF
 
 # copy fix data
