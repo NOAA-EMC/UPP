faabdf11eb2dfca4157d6cb1ea72beb19b673c1e
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-02-05 10:05:53 -0800

    Update CALLCL.f

--- sorc/ncep_post.fd/CALLCL.f
+++ sorc/ncep_post.fd/CALLCL.f
@@ -99,6 +99,7 @@
         DALP      = ALPINT(I,J,L) - ALPINT(I,J,L+1)
         DZ        = ZINT(I,J,L)   - ZINT(I,J,L+1)
         ZLCL(I,J) = max(D00, ZINT(I,J,L+1) + DZ*DLPLCL/DALP - ZSFC)
+        print *, 'L =', L
         EXIT
       ENDIF
  20   CONTINUE
