16be116602c5cc134532ed8b4b645e9312f6d67a
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-02-05 15:14:40 -0800

    Update test_callcl.f90

--- unit_tests/test_callcl.f90
+++ unit_tests/test_callcl.f90
@@ -15,9 +15,11 @@ program test_callcl
     real, parameter :: tol = 1.0e-6
     ! From CALLCL.f
     real, parameter :: D35=3.5, D4805=4.805,  H2840=2840., H55=55., D2845=0.2845, D28=0.28
+    ! Used to initialize inputs
+    real, parameter :: zsfc = 10.0, dz = 200.0, Hscale = 8000.0
     integer, parameter :: npts = 2, nlevs = 60
     integer :: i, j, k, res
-    real :: zsfc(1:npts,1:npts), levels(1:nlevs), pint(1:npts,1:npts,1:nlevs)
+    real :: z, p
     real :: P1D(1:npts,1:npts), T1D(1:npts,1:npts), Q1D(1:npts,1:npts)
     real :: PLCL(1:npts,1:npts), ZLCL(1:npts,1:npts)
     real :: EXP_PLCL(1:npts,1:npts), EXP_ZLCL(1:npts,1:npts)
@@ -45,12 +47,29 @@ program test_callcl
     ! Set to nlevs - 1 since loop accesses lmh(i,j) + 1
     lmh = nlevs - 1
 
-    fis = 980.0      ! Geopotential at surface (~100 m AGL if GI≈1/g)
-    P1D = 95000.0    ! Surface parcel pressure (Pa), ~950 hPa
-    T1D = 300.0      ! Parcel temperature (K), ~27°C
-    Q1D = 0.012      ! Specific humidity (kg/kg), ~12 g/kg
+    ! TODO: Replace ??? with a single value for all points of the input arrays, fis, P1D, T1D, Q1D. The selected 
+    ! values should be physically reasonable and lead to a non-trivial test of the CALLCL() logic.
+    ! The selected values should also:
+    ! - Not clip ARG to H1M12 on line 89 of CALLCL.f
+    ! - Not clip ZLCL to D00 on line 101 of CALLCL.f
+    ! - No Values set to zero
+    fis = zsfc/gi        ! Set surface geopotential consistent with ~10 m AGL
+    P1D = 100000.0       ! 1000 hPa surface parcel pressure (Pa)
+    T1D = 300.0          ! 300 K surface parcel temperature
+    Q1D = 0.012          ! 12 g/kg specific humidity (kg/kg)
 
-    
+    ! TODO: Replace ??? with code that initializes alpint and zint with physically reasonable values that are 
+    ! consistent with the selected P1D, T1D, Q1D, and fis values. The selected values should lead to a non-trivial 
+    ! test of the CALLCL() logic.
+    ! The selected values should also:
+    ! - Not clip ARG to H1M12 on line 89 of CALLCL.f
+    ! - Not clip ZLCL to D00 on line 101 of CALLCL.f
+    ! - No Values set to zero
+
+    do k = 1, nlevs
+        zint(:,:,k) = zsfc + (k-1)*dz
+        alpint(:,:,k) = log(100000) - zint(:,:,k)/Hscale
+    end do
 
     call callcl(P1D, T1D, Q1D, PLCL, ZLCL)
 
