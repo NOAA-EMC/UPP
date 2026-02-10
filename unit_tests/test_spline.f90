78b60a28ec25b3eb9ee6b2a2b7dadf6d1a872eec
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-02-10 14:21:52 -0800

    Update test_spline.f90

--- unit_tests/test_spline.f90
+++ unit_tests/test_spline.f90
@@ -7,13 +7,11 @@ program test_spline
     implicit none
 
     real, parameter :: tol = 1.0e-8
-    ! TODO: Replace ??? with the actual value for JTB, which is the maximum size of the arrays used in the SPLINE subroutine.
-    ! Should be as large as is strictly necessary to accommodate the test cases.
     integer, parameter :: JTB = 5
     integer :: i, res
     integer :: NOLD, NNEW
     real :: XOLD(JTB), YOLD(JTB), XNEW(JTB), P(JTB), Q(JTB), Y2(JTB)
-    real :: YNEW(JTB)
+    real :: YNEW(JTB), EXP_YNEW(JTB)
 
     interface
         subroutine SPLINE(JTB,NOLD,XOLD,YOLD,Y2,NNEW,XNEW,YNEW,P,Q)
@@ -24,22 +22,14 @@ program test_spline
         end subroutine SPLINE
     end interface
 
-    ! TODO: Replace the ??? with code the defines all of the input values for the SPLINE() subroutine. 
-    ! The values should be chosen such that as many lines of the SPLINE() subroutine as possible are executed.
-    ! Note that not all cases can be executed at once since there are early exit conditions. Your job is 
-    ! only to select values that will execute as many lines as possible in a single run of the SPLINE() subroutine.
     ! Choose NOLD>3 to exercise forward sweep; NNEW>=2 to test reuse/recompute
     NOLD = 5
     NNEW = 4
 
     ! Ascending, nonuniform XOLD to exercise variable spacing
-    XOLD(1) = 0.0
-    XOLD(2) = 1.0
-    XOLD(3) = 2.5
-    XOLD(4) = 4.0
-    XOLD(5) = 5.0
+    XOLD = (/0.0, 1.0, 2.5, 4.0, 5.0/)
 
-    ! Nonlinear YOLD values; use a simple quadratic for determinism
+    ! Nonlinear YOLD values
     do i = 1, NOLD
         YOLD(i) = XOLD(i)**2
     end do
@@ -56,5 +46,9 @@ program test_spline
     XNEW(4) = XOLD(NOLD)   ! right endpoint path (assign YNEW=YOLD(NOLD))
 
     call SPLINE(JTB,NOLD,XOLD,YOLD,Y2,NNEW,XNEW,YNEW,P,Q)
+
+    do i = 1, JTB
+        print '(A,I0,A,ES24.10)', 'YNEW(', i, ') = ', YNEW(i)
+    end do
     print *, 'SUCCESS!'
 end program test_spline
