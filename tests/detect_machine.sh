42a6608d99346cb7e827523fab477f129a7dc1cc
Wei Huang <Wei.Huang@noaa.gov>
2026-02-24 00:21:30 +0000

    compile on AWS EC2

--- tests/detect_machine.sh
+++ tests/detect_machine.sh
@@ -33,6 +33,8 @@ case $(hostname -f) in
 
   s4-submit.ssec.wisc.edu) MACHINE_ID=s4 ;; ### s4
 
+  ip-*) MACHINE_ID=aws-ec2 ;; ### aws-ec2
+
   fe[1-8]) MACHINE_ID=jet ;; ### jet1-8
   tfe[12]) MACHINE_ID=jet ;; ### tjet1-2
 
@@ -110,9 +112,12 @@ elif [[ -d /gpfs/f6 ]]; then
 elif [[ -d /data/prod ]]; then
   # We are on SSEC's S4
   MACHINE_ID=s4
-elif [[ -d /opt/spack-stack && -v SINGULARITY_CONTAINER ]]; then
+elif [[ -v SINGULARITY_CONTAINER ]]; then
   # We are in a container
   MACHINE_ID=container
+elif [[ -d /opt/spack-stack ]]; then
+  # We are on aws-ec2
+  MACHINE_on=aws-ec2
 else
   echo WARNING: UNKNOWN PLATFORM 1>&2
 fi
