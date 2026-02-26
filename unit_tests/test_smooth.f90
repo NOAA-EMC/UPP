e371e0b5a40d7766aceac9a364cb6edbb8362143
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-02-26 12:47:14 -0800

    Update test_smooth.f90

--- unit_tests/test_smooth.f90
+++ unit_tests/test_smooth.f90
@@ -33,8 +33,13 @@ program test_smooth
     SMTH = 0.5
     HOLD = 0.0
 
-    do j = 1, ny
-        do i = 1, nx
+    EXP_FIELD = reshape([280.0, 281.0, 282.0, 283.0, &
+        282.0, 283.0, 284.0, 285.0, &
+        284.0, 285.0, 286.0, 287.0, &
+        286.0, 287.0, 288.0, 289.0], [nx, ny]) 
+
+    do i = 1, nx
+        do j = 1, ny
             FIELD(i, j) = 280.0 + 2.0 * real(j - 1) + real(i - 1)
             FIELDC(i, j) = 280.0 + 2.0 * real(j - 1) + real(i - 1)
         end do
@@ -44,13 +49,20 @@ program test_smooth
     
     call SMOOTH(FIELD, HOLD, IX, IY, SMTH)
 
-    print *, "SMOOTH() Results:"
+    res = 0
     do i = 1, nx
         do j = 1, ny
-            print '("(",I1,",",I1,"): ",ES24.10)', i, j, FIELD(i, j)
+            if (abs(FIELD(i,j) - EXP_FIELD(i,j)) > tol) then
+                print *, 'FIELD Test failed at (', i, ',', j, '): ', &
+                         'Expected ', EXP_FIELD(i,j), &
+                         ' but got ', FIELD(i,j)
+                res = 1
+            end if
         end do
     end do  
 
+    if (res .ne. 0) stop 10
+    
     ! Test for SMOOTHC()
 
     HOLD = 0.0 ! Reset HOLD for SMOOTHC test, just in case.
