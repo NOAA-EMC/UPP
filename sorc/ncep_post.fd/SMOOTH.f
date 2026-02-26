6b6d6a44d4493418a006002fc56cd4e4e613c836
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-02-26 13:30:12 -0800

    Update SMOOTH.f

--- sorc/ncep_post.fd/SMOOTH.f
+++ sorc/ncep_post.fd/SMOOTH.f
@@ -150,12 +150,14 @@
       SMTH4 = (1.-SMTH)
       SMTH5 = 0.5 * SMTH
 !
-      do i=1,ix-1
+      do i=2,ix-1
         ie(i) = i + 1
         iw(i) = i - 1
       enddo
+      ie(1) = 2
       ie(ix) = 1
       iw(1)  =  ix
+      iw(ix) = ix - 1
 !
       I1 = 2
       I2 = 1
