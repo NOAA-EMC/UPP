8c819815f9ec7ba1d50b9f6eeec5dfa6295ffbd4
Wen Meng <wen.meng@noaa.gov>
2026-03-27 12:34:56 +0000

    Update modulefile for WCOSS2

--- modulefiles/wcoss2_intel.lua
+++ modulefiles/wcoss2_intel.lua
@@ -1,11 +1,11 @@
 help([[
-Load environment to build post on WCOSS2
+Load environment to build UPP on WCOSS2
 ]])
 
-PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
+PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.5.0"
 intel_ver=os.getenv("intel_ver") or "19.1.3.304"
-craype_ver=os.getenv("craype_ver") or "2.7.10"
-cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
+craype_ver=os.getenv("craype_ver") or "2.7.17"
+cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.19"
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
 load(pathJoin("craype", craype_ver))
@@ -14,39 +14,31 @@ load(pathJoin("cray-mpich", cray_mpich_ver))
 cmake_ver=os.getenv("cmake_ver") or "3.20.2"
 load(pathJoin("cmake", cmake_ver))
 
-hdf5_ver=os.getenv("hdf5_ver") or "1.10.6"
-netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
-load(pathJoin("hdf5", hdf5_ver))
-load(pathJoin("netcdf", netcdf_ver))
+local upp_modules = {
+  {["jasper"]          = "2.0.25" },
+  {["zlib"]            = "1.2.11"  },
+  {["libpng"]          = "1.6.37" },
+  {["hdf5-D"]          = "1.14.0" },
+  {["netcdf-D"]        = "4.9.2"  },
+  {["pnetcdf-D"]       = "1.12.2"  },
+  {["bacio"]           = "2.4.1"  },
+  {["crtm"]            = "2.4.0.1"},
+  {["g2"]              = "3.5.1"  },
+  {["g2tmpl"]          = "1.17.0" },
+  {["ip"]              = "4.0.0"  },
+  {["sp"]              = "2.3.3"  },
+  {["w3emc"]           = "2.12.0" },
+  {["nemsio"]          = "2.5.2"  },
+  {["sigio"]           = "2.3.2"  },
+  {["wrf_io"]          = "1.2.0"  },
+}
 
-jasper_ver=os.getenv("jasper_ver") or "2.0.25"
-libpng_ver=os.getenv("libpng_ver") or "1.6.37"
-zlib_ver=os.getenv("zlib_ver") or "1.2.11"
-load(pathJoin("jasper", jasper_ver))
-load(pathJoin("libpng", libpng_ver))
-load(pathJoin("zlib", zlib_ver))
-
-g2_ver=os.getenv("g2_ver") or "3.5.1"
-g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.17.0"
-bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-ip_ver=os.getenv("ip_ver") or "4.0.0"
-sp_ver=os.getenv("sp_ver") or "2.3.3"
-crtm_ver=os.getenv("crtm_ver") or "2.4.0.1"
-w3emc_ver=os.getenv("w3emc_ver") or "2.12.0"
-load(pathJoin("g2", g2_ver))
-load(pathJoin("g2tmpl", g2tmpl_ver))
-load(pathJoin("bacio", bacio_ver))
-load(pathJoin("ip", ip_ver))
-load(pathJoin("sp", sp_ver))
-load(pathJoin("crtm", crtm_ver))
-load(pathJoin("w3emc", w3emc_ver))
-
-nemsio_ver=os.getenv("nemsio_ver") or "2.5.2"
-sigio_ver=os.getenv("sigio_ver") or "2.3.2"
-wrf_io_ver=os.getenv("wrf_io_ver") or "1.2.0"
-load(pathJoin("nemsio", nemsio_ver))
-load(pathJoin("sigio", sigio_ver))
-load(pathJoin("wrf_io", wrf_io_ver))
+for i = 1, #upp_modules do
+  for name, default_version in pairs(upp_modules[i]) do
+    local env_version_name = string.gsub(name, "-", "_") .. "_ver"
+    load(pathJoin(name, os.getenv(env_version_name) or default_version))
+  end
+end
 
 setenv("CC","cc")
 setenv("CXX","CC")
