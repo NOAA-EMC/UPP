4327170a3a2d901f0021592f98e28b9928ffbaff
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-04-27 14:12:43 -0700

    Update test_assignnemsiovar.f90

--- unit_tests/test_assignnemsiovar.f90
+++ unit_tests/test_assignnemsiovar.f90
@@ -7,18 +7,18 @@ program test_assignnemsiovar
     use ctlblk_mod, only: me
     implicit none
 
-    interface
-        subroutine ASSIGNNEMSIOVAR(IM, JSTA, JEND, JSTA, JSTA_2L, JEND_2U, &
-                                    L, NREC, FLDSIZE, SPVAL, TMP, RECNAME, &
-                                    RECLEVTYP, RECLEV, VARNAME, VCOORDNAME, BUF)
-            integer, intent(in) :: IM, JSTA, JEND, JSTA_2L, JEND_2U, L, NREC, FLDSIZE
-            integer, intent(in) :: RECLEV(NREC)
-            real, intent(in) :: SPVAL, TMP(FLDSIZE * NREC)
-            character(*), intent(in) :: RECNAME(NREC), RECLEVTYP(NREC)
-            character(*), intent(in) :: VARNAME, VCOORDNAME
-            real, intent(out) :: BUF(IM, JSTA_2L:JEND_2U)
-        end subroutine ASSIGNNEMSIOVAR
-    end interface
+    !interface
+    !    subroutine ASSIGNNEMSIOVAR(IM, JSTA, JEND, JSTA, JSTA_2L, JEND_2U, &
+   !                                 L, NREC, FLDSIZE, SPVAL, TMP, RECNAME, &
+   !                                 RECLEVTYP, RECLEV, VARNAME, VCOORDNAME, BUF)
+   !         integer, intent(in) :: IM, JSTA, JEND, JSTA_2L, JEND_2U, L, NREC, FLDSIZE
+    !        integer, intent(in) :: RECLEV(NREC)
+    !        real, intent(in) :: SPVAL, TMP(FLDSIZE * NREC)
+    !        character(*), intent(in) :: RECNAME(NREC), RECLEVTYP(NREC)
+    !        character(*), intent(in) :: VARNAME, VCOORDNAME
+    !        real, intent(out) :: BUF(IM, JSTA_2L:JEND_2U)
+    !    end subroutine ASSIGNNEMSIOVAR
+    !end interface
 
 
     print *, "SUCCESS!"
