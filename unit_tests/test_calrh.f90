8264f2bbaebc18b47faa8022e2b694f6391444f8
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-05-14 15:43:53 -0700

    check what happens to calrh_nam() if Q or P have spval

--- unit_tests/test_calrh.f90
+++ unit_tests/test_calrh.f90
@@ -1,30 +1,26 @@
 ! This is a test program for UPP.
 !
-! This program tests the subroutines CALRH(), CALRH_GSD() and CALRH_NAM()
+! This program tests the subroutines CALRH(), CALRH_GSD() and CALRH()
 ! in the upp_physics module.
 !
 ! Alyson Stahl, 5/2026
 program test_calrh
-    use upp_physics, only: CALRH, CALRH_GSD, CALRH_NAM
+    use upp_physics, only: CALRH_NAM, CALRH_GSD, CALRH
     use ctlblk_mod, only: ista, iend, jsta, jend, spval, modelname
     use params_mod, only: PQ0, a2, a3, a4, rhmin
     implicit none
 
     real, parameter :: tol = 1.0e-8
-    integer, parameter :: npts = 7
+    integer, parameter :: npts = 9
     integer :: i, res
     ! For expected value calculations
     real :: QC
-    ! Keeping arrays separate because Q1 array can get overwritten.
-    ! Arrays for CALRH_NAM()
-    real :: P1_NAM(1, npts), T1_NAM(1, npts)
-    real :: Q1_NAM(1, npts), EXP_Q1_NAM(1, npts)
-    real :: RH_NAM(1, npts), EXP_RH_NAM(1, npts)
-    ! Arrays for CALRH_GSD()
-    real :: P1_GSD(1, npts), T1_GSD(1, npts), Q1_GSD(1, npts)
-    real :: RH_GSD(1, npts), EXP_RH_GSD(1, npts)
-    ! Arrays for CALRH()
-    real :: P1(1, npts), T1(1, npts), Q1(1, npts), RH(1, npts)
+    real :: P1(1, npts), T1(1, npts), RH(1, npts)
+    ! Keeping Q1 arrays separate because it can get overwritten.
+    real :: Q1_NAM(1, npts), Q1_GSD(1, npts), Q1(1, npts)
+    real :: EXP_Q1_NAM(1, npts), EXP_Q1_GSD(1, npts), 
+    real :: EXP_RH(1, npts), EXP_RH_GSD(1, npts)
+
 
     ! Grid dimensions
     ista = 1
@@ -37,93 +33,102 @@ program test_calrh
     print *, "Testing CALRH_NAM()..."
 
     ! Test Case 1: Standard case where RHmin < RH (Q1/QC) < 1.0 (default case)
-    P1_NAM = 85000.0
-    T1_NAM = 280.0
+    P1 = 85000.0
+    T1 = 280.0
     Q1_NAM = 0.004
-    RH_NAM = 0.0 ! Give a default value.
+    RH = 0.0 ! Give a default value.
     EXP_Q1_NAM = 0.004
 
-    QC = PQ0/P1_NAM(1,1)*EXP(A2*(T1_NAM(1,1)-A3)/(T1_NAM(1,1)-A4))
-    EXP_RH_NAM = Q1_NAM(1,1)/QC
+    QC = PQ0/P1(1,1)*EXP(A2*(T1(1,1)-A3)/(T1(1,1)-A4))
+    EXP_RH = Q1_NAM(1,1)/QC
     
     ! Test Case 2: Q1/QC > 1.0 (Clipped to 1.0)
     Q1_NAM(1,2) = 0.010
     EXP_Q1_NAM(1,2) = QC
-    EXP_RH_NAM(1,2) = 1.0
+    EXP_RH(1,2) = 1.0
 
     ! Test Case 3: Q1/QC < RHmin & P1 >= 300.0 (Clipped to RHmin)
     Q1_NAM(1,3) = 1e-10
     EXP_Q1_NAM(1,3) = RHMIN * QC
-    EXP_RH_NAM(1,3) = RHMIN
+    EXP_RH(1,3) = RHMIN
 
     ! Test Case 4: RHmin / 10 < Q1/QC < RHmin & P1 < 300.0 
-    P1_NAM(1,4) = 100.0
+    P1(1,4) = 100.0
     Q1_NAM(1,4) = 1e-6
-    QC = PQ0/P1_NAM(1,4)*EXP(A2*(T1_NAM(1,4)-A3)/(T1_NAM(1,4)-A4))
-    EXP_RH_NAM(1,4) = Q1_NAM(1,4)/QC
-    EXP_Q1_NAM(1,4) = EXP_RH_NAM(1,4) * QC
+    QC = PQ0/P1(1,4)*EXP(A2*(T1(1,4)-A3)/(T1(1,4)-A4))
+    EXP_RH(1,4) = Q1_NAM(1,4)/QC
+    EXP_Q1_NAM(1,4) = EXP_RH(1,4) * QC
 
     ! Test Case 5: Q1/QC < RHmin / 10 & P1 < 300.0 (Clipped to RHmin / 10)
-    P1_NAM(1,5) = 100.0
+    P1(1,5) = 100.0
     Q1_NAM(1,5) = 1e-8
-    EXP_RH_NAM(1,5) = RHMIN / 10
-    QC = PQ0/P1_NAM(1,5)*EXP(A2*(T1_NAM(1,5)-A3)/(T1_NAM(1,5)-A4))
-    EXP_Q1_NAM(1,5) = EXP_RH_NAM(1,5) * QC
+    EXP_RH(1,5) = RHMIN / 10
+    QC = PQ0/P1(1,5)*EXP(A2*(T1(1,5)-A3)/(T1(1,5)-A4))
+    EXP_Q1_NAM(1,5) = EXP_RH(1,5) * QC
 
     ! Test Case 6: ABS(P1) < 1 (RH is not set to anything)
-    P1_NAM(1,6) = 0.5
-    EXP_RH_NAM(1,6) = 0.0
+    P1(1,6) = 0.5
+    EXP_RH(1,6) = 0.0
 
     ! Test Case 7: T1 has spval
-    T1_NAM(1,7) = spval
-    EXP_RH_NAM(1,7) = spval
+    T1(1,7) = spval
+    EXP_RH(1,7) = spval
 
-    call CALRH_NAM(P1_NAM, T1_NAM, Q1_NAM, RH_NAM)
+    ! Test Case 8: P1 has spval
+    P1(1,8) = spval
+
+    ! Test Case 9: Q1 has spval
+    Q1_NAM(1,9) = spval
+    
+    call CALRH_NAM(P1, T1, Q1_NAM, RH)
 
     res = 0
     do i = 1, npts
-        if (abs(Q1_NAM(1,i) - EXP_Q1_NAM(1,i)) > tol) then
-            print *, "CALRH_NAM() Failed for test ", i, ": ", &
-                        "Expected Q1 = ", EXP_Q1_NAM(1,i), &
-                                " but got Q1 = ", Q1_NAM(1,i)
-            res = 1
-        end if
-        if (abs(RH_NAM(1,i) - EXP_RH_NAM(1,i)) > tol) then
-            print *, "CALRH_NAM() Failed for test ", i, ": ", &
-                        "Expected RH = ", EXP_RH_NAM(1,i), &
-                        " but got RH = ", RH_NAM(1,i)
-            res = 1
-        end if
+        print '(A,I0,A,ES24.10)', "RH(", i, ") = ", RH(1,i)
+        !if (abs(Q1_NAM(1,i) - EXP_Q1_NAM(1,i)) > tol) then
+        !    print *, "CALRH_NAM() Failed for test ", i, ": ", &
+        !                "Expected Q1 = ", EXP_Q1_NAM(1,i), &
+        !                        " but got Q1 = ", Q1_NAM(1,i)
+        !    res = 1
+        !end if
+        !if (abs(RH(1,i) - EXP_RH(1,i)) > tol) then
+        !    print *, "CALRH_NAM() Failed for test ", i, ": ", &
+        !                "Expected RH = ", EXP_RH(1,i), &
+        !                        " but got RH = ", RH(1,i)
+        !    res = 1
+        !end if
     end do
 
     if (res .ne. 0) stop 10
 
     print *, "Testing CALRH_GSD()..."
 
-    P1_GSD = 85000.0
-    T1_GSD = 280.0
+    !P1_GSD = 85000.0
+    !T1_GSD = 280.0
     Q1_GSD = 0.004
-    RH_GSD = 0.0 ! Give a default value.
+    RH = 0.0 ! Give a default value.
+    !RH_GSD = 0.0 ! Give a default value.
 
     Q1_GSD(1,2) = 0.010
 
     Q1_GSD(1,3) = 1e-10
 
-    P1_GSD(1,4) = 100.0
+    !P1_GSD(1,4) = 100.0
     Q1_GSD(1,4) = 1e-6
 
-    P1_GSD(1,5) = 100.0
+    !P1_GSD(1,5) = 100.0
     Q1_GSD(1,5) = 1e-8
 
-    P1_GSD(1,6) = 0.5
+    !P1_GSD(1,6) = 0.5
 
-    T1_GSD(1,7) = spval
+    !T1_GSD(1,7) = spval
 
-    call CALRH_GSD(P1_GSD, T1_GSD, Q1_GSD, RH_GSD)
+    !call CALRH_GSD(P1_GSD, T1_GSD, Q1_GSD, RH_GSD)
+    call CALRH_GSD(P1, T1, Q1_GSD, RH)
 
     do i = 1, npts
         print '(A,I0,A,ES24.10)', "Q1(", i, ") = ", Q1_GSD(1,i)
-        print '(A,I0,A,ES24.10)', "RH(", i, ") = ", RH_GSD(1,i)
+        print '(A,I0,A,ES24.10)', "RH(", i, ") = ", RH(1,i)
     end do
 
 end program test_calrh
