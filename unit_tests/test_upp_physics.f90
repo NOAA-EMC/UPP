16157c7c7e909dc835b986678e6a322fee3486c0
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-05-14 12:29:29 -0700

    add tests for upp_physics module functions

---
+++ unit_tests/test_upp_physics.f90
@@ -0,0 +1,101 @@
+! This is a test program for UPP.
+!
+! This program tests some of the smaller functions and subroutines in 
+! the upp_physics module.
+!
+! Tested functions/subroutines:
+!   - TVIRTUAL()
+!   - FPVSNEW()
+!
+! Alyson Stahl, 5/2026
+program test_upp_physics
+    use upp_physics, only: FPVSNEW, TVIRTUAL
+    implicit none
+
+    real, parameter :: tol = 1.0e-8
+    ! Number of test points for TVIRTUAL(). 
+    integer, parameter :: N_TV = 5
+    ! Number of test points for FPVSNEW().
+    integer, parameter :: N_FPVS = 5
+    !
+    real :: T(N_TV), Q(N_TV), TV(N_TV), EXP_TV(N_TV)
+    real :: T_FPVS(N_FPVS), EXP_SVP(N_FPVS), SVP(N_FPVS)
+    !
+    integer :: i, res
+
+    print *, "Testing TVIRTUAL()..."
+
+    ! No branches or edge cases in TVIRTUAL(), so just covering a range a reasonable inputs.
+    T(1) = 0.0
+    Q(1) = 0.0
+    EXP_TV(1) = 0.0
+
+    T(2) = 220.0
+    Q(2) = 0.0
+    EXP_TV(2) = 220.0
+
+    T(3) = 280.0
+    Q(3) = 0.005
+    EXP_TV(3) = 280.8512
+
+    T(4) = 295.0
+    Q(4) = 0.0150
+    EXP_TV(4) = 297.6904
+
+    T(5) = 305.0
+    Q(5) = 0.0250
+    EXP_TV(5) = 309.636
+
+    TV = TVIRTUAL(T, Q)
+
+    res = 0
+    do i = 1, N_TV
+        if (abs(TV(i) - EXP_TV(i)) > tol) then
+            print *, "TVIRTUAL() Failed for test ", i, ": ", &
+                        "Expected ", EXP_TV(i), &
+                        " but got ", TV(i)
+            res = 1
+        end if
+    end do
+
+    if (res .ne. 0) stop 10
+
+    print *, "Testing FPVSNEW()..."
+
+    ! Test Case 1: x, xp1 < tice
+    T_FPVS(1) = 200.0
+    EXP_SVP(1) = 0.0 ! TODO: Set exp value
+
+    ! Test Case 2: x == tice
+    T_FPVS(2) = 253.16
+    EXP_SVP(2) = 0.0 ! TODO: Set exp value
+
+    ! Test Case 3: tice < x, xp1 < tliq
+    T_FPVS(3) = 270.0
+    EXP_SVP(3) = 0.0 ! TODO: Set exp value
+
+    ! Test Case 4: x == tliq
+    T_FPVS(4) = 273.16
+    EXP_SVP(4) = 0.0 ! TODO: Set exp value
+
+    ! Test Case 5: x, xp1 > tliq
+    T_FPVS(5) = 290.0
+    EXP_SVP(5) = 0.0 ! TODO: Set exp value
+
+    SVP = FPVSNEW(T_FPVS)
+
+    res = 0
+    do i = 1, N_FPVS
+        print '(A,I0,A,ES24.10)', "SVP(", i, ") = ", SVP(i)
+        !if (abs(SVP(i) - EXP_SVP(i)) > tol) then
+        !    print *, "FPVSNEW() Failed for test ", i, ": ", &
+        !                "Expected ", EXP_SVP(i), &
+        !                " but got ", SVP(i)
+        !    res = 1
+        !end if
+    end do
+
+    if (res .ne. 0) stop 20
+    
+    print *, "SUCCESS!"
+end program test_upp_physics
