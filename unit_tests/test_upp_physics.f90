afad7129b3773b853575beded899e35f1ded05fc
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-05-14 13:05:35 -0700

    set expect values

--- unit_tests/test_upp_physics.f90
+++ unit_tests/test_upp_physics.f90
@@ -12,7 +12,7 @@ program test_upp_physics
     use upp_physics, only: FPVSNEW, TVIRTUAL
     implicit none
 
-    real, parameter :: tol = 1.0e-6
+    real, parameter :: tol = 1.0e-8
     ! Number of test points for TVIRTUAL(). 
     integer, parameter :: N_TV = 5
     ! Number of test points for FPVSNEW().
@@ -36,15 +36,15 @@ program test_upp_physics
 
     T(3) = 280.0
     Q(3) = 0.005
-    EXP_TV(3) = 280.8512
+    EXP_TV(3) = 280.0 * (1 + 0.608 * 0.005)
 
     T(4) = 295.0
     Q(4) = 0.015
-    EXP_TV(4) = 297.6904
+    EXP_TV(4) = 295.0 * (1 + 0.608 * 0.015)
 
     T(5) = 305.0
     Q(5) = 0.025
-    EXP_TV(5) = 309.636
+    EXP_TV(5) = 305.0 * (1 + 0.608 * 0.025)
 
     TV = TVIRTUAL(T, Q)
 
@@ -64,35 +64,35 @@ program test_upp_physics
 
     ! Test Case 1: x, xp1 < tice
     T_FPVS(1) = 200.0
-    EXP_SVP(1) = 0.0 ! TODO: Set exp value
+    EXP_SVP(1) = 1.5914107859E-01
 
     ! Test Case 2: x == tice
     T_FPVS(2) = 253.16
-    EXP_SVP(2) = 0.0 ! TODO: Set exp value
+    EXP_SVP(2) = 1.0326692963E+02
 
     ! Test Case 3: tice < x, xp1 < tliq
     T_FPVS(3) = 270.0
-    EXP_SVP(3) = 0.0 ! TODO: Set exp value
+    EXP_SVP(3) = 4.8179052734E+02
 
     ! Test Case 4: x == tliq
     T_FPVS(4) = 273.16
-    EXP_SVP(4) = 0.0 ! TODO: Set exp value
+    EXP_SVP(4) = 6.1078002930E+02
 
     ! Test Case 5: x, xp1 > tliq
     T_FPVS(5) = 290.0
-    EXP_SVP(5) = 0.0 ! TODO: Set exp value
+    EXP_SVP(5) = 1.9149111328E+03
 
     SVP = FPVSNEW(T_FPVS)
 
     res = 0
     do i = 1, N_FPVS
-        print '(A,I0,A,ES24.10)', "SVP(", i, ") = ", SVP(i)
-        !if (abs(SVP(i) - EXP_SVP(i)) > tol) then
-        !    print *, "FPVSNEW() Failed for test ", i, ": ", &
-        !                "Expected ", EXP_SVP(i), &
-        !                " but got ", SVP(i)
-        !    res = 1
-        !end if
+        !print '(A,I0,A,ES24.10)', "SVP(", i, ") = ", SVP(i)
+        if (abs(SVP(i) - EXP_SVP(i)) > tol) then
+            print *, "FPVSNEW() Failed for test ", i, ": ", &
+                        "Expected ", EXP_SVP(i), &
+                        " but got ", SVP(i)
+            res = 1
+        end if
     end do
 
     if (res .ne. 0) stop 20
