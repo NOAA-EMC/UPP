613121d5334ed4a5cfcb31aaf623f8ab99039280
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-05-27 18:08:01 -0700

    Update test_frzlvl.f90

--- unit_tests/test_frzlvl.f90
+++ unit_tests/test_frzlvl.f90
@@ -155,9 +155,9 @@ program test_frzlvl
     call FRZLVL(ZFRZ, RHFRZ, PFRZL)
 
     do i = 1, npts
-        print '(A, I0, A, ES16.9)', "EXP_ZFRZ(1, ", i, ") = ", EXP_ZFRZ(1, i)
-        print '(A, I0, A, ES16.9)', "EXP_RHFRZ(1, ", i, ") = ", EXP_RHFRZ(1, i)
-        print '(A, I0, A, ES16.9)', "EXP_PFRZL(1, ", i, ") = ", EXP_PFRZL(1, i)
+        print '(A, I0, A, ES16.9)', "EXP_ZFRZ(1, ", i, ") = ", ZFRZ(1, i)
+        print '(A, I0, A, ES16.9)', "EXP_RHFRZ(1, ", i, ") = ", RHFRZ(1, i)
+        print '(A, I0, A, ES16.9)', "EXP_PFRZL(1, ", i, ") = ", PFRZL(1, i)
     end do
 
     if (res .ne. 0) then
