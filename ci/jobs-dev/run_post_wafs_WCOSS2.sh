e99bc4fe7644d511478aad1c4343de478c836b8e
benjamin blake <benjamin.blake@dlogin02.dogwood.wcoss2.ncep.noaa.gov>
2026-05-28 14:05:59 +0000

    Update path to data_in for WAFS

--- ci/jobs-dev/run_post_wafs_WCOSS2.sh
+++ ci/jobs-dev/run_post_wafs_WCOSS2.sh
@@ -54,13 +54,13 @@ export HH=`echo $NEWDATE | cut -c9-10`
 
 cat > itag <<EOF
 &model_inputs
-fileName='$homedir/data_in/gfs/gfs.t${cyc}z.atmf${fhr}.nc'
+fileName='$homedir/data_in/wafs/gfs.t${cyc}z.atmf${fhr}.nc'
 IOFORM='netcdf'
 grib='grib2'
 DateStr='${YY}-${MM}-${DD}_${HH}:00:00'
 MODELNAME='GFS'
 SUBMODELNAME='GFS'
-fileNameFlux='$homedir/data_in/gfs/gfs.t${cyc}z.sfcf${fhr}.nc'
+fileNameFlux='$homedir/data_in/wafs/gfs.t${cyc}z.sfcf${fhr}.nc'
 /
 &NAMPGB
 KPO=60,PO=97720.,94210.,90810.,87510.,84310.,81200.,78190.,75260.,72430.,69680.,67020.,64440.,61940.,59520.,57180.,54920.,52720.,50600.,48550.,46560.,44650.,42790.,41000.,39270.,37600.,35990.,34430.,32930.,31490.,30090.,28740.,27450.,26200.,25000.,23840.,22730.,21660.,20650.,19680.,18750.,17870.,17040.,16240.,15470.,14750.,14060.,13400.,12770.,12170.,11600.,11050.,10530.,10040.,9570.,9120.,8700.,8280.,7900.,7520.,7170.,gtg_on=.true.,popascal=.true.,
